36e790c18c3cd81b9d1dd368f270d48e0eb9c8c1
kowatsch <kowatsch@heigit4.geog.uni-heidelberg.de>
2018-02-22 09:30:54 +0100

    implementing of redirect to swagger-ui when accessing server root path (e.g.: http://localhost:8080)

--- src/main/java/org/heigit/bigspatialdata/ohsome/oshdbRestApi/config/ElementsMvcConfig.java
+++ src/main/java/org/heigit/bigspatialdata/ohsome/oshdbRestApi/config/ElementsMvcConfig.java
@@ -4,6 +4,7 @@ import org.heigit.bigspatialdata.ohsome.oshdbRestApi.interceptor.ElementsRequest
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
@@ -22,4 +23,8 @@ public class ElementsMvcConfig extends WebMvcConfigurerAdapter {
     registry.addInterceptor(elementsRequestInterceptor).addPathPatterns("/**/elements/**/");
   }
 
+  @Override
+  public void addViewControllers(ViewControllerRegistry registry) {
+    registry.addRedirectViewController("/", "/swagger-ui.html");
+  }
 }
