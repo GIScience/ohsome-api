839de58aa72680e6a77827924db5cdf8093c9a7b
kowatsch <kowatsch@heigit4.geog.uni-heidelberg.de>
2018-03-02 16:12:23 +0100

    implementation of custom error-json response and new error response class
    error response class NotFoundException with a customized json response

--- src/main/java/org/heigit/bigspatialdata/ohsome/ohsomeApi/config/ElementsMvcConfig.java
+++ src/main/java/org/heigit/bigspatialdata/ohsome/ohsomeApi/config/ElementsMvcConfig.java
@@ -1,8 +1,14 @@
 package org.heigit.bigspatialdata.ohsome.ohsomeApi.config;
 
+import java.time.LocalDateTime;
+import java.util.Map;
 import org.heigit.bigspatialdata.ohsome.ohsomeApi.interceptor.ElementsRequestInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.DefaultErrorAttributes;
+import org.springframework.boot.autoconfigure.web.ErrorAttributes;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -10,7 +16,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 /**
  * Configuration class, which adds the
  * {@link org.heigit.bigspatialdata.ohsome.ohsomeApi.interceptor.ElementsRequestInterceptor
- * ElementsRequestInterceptor} class into the spring MVC life cycle.
+ * ElementsRequestInterceptor} class into the spring MVC life cycle and modifies the error-JSON
+ * response.
  */
 @Configuration
 public class ElementsMvcConfig extends WebMvcConfigurerAdapter {
@@ -26,5 +33,25 @@ public class ElementsMvcConfig extends WebMvcConfigurerAdapter {
   @Override
   public void addViewControllers(ViewControllerRegistry registry) {
     registry.addRedirectViewController("/", "/swagger-ui.html");
+  }
+
+  @Bean
+  public ErrorAttributes modifyExceptionResponse() {
+    return new DefaultErrorAttributes() {
+      @Override
+      public Map<String, Object> getErrorAttributes(RequestAttributes requestAttributes,
+          boolean includeStackTrace) {
+        Map<String, Object> errorAttributes =
+            super.getErrorAttributes(requestAttributes, includeStackTrace);
+
+        errorAttributes.remove("path");
+        errorAttributes.remove("exception");
+        errorAttributes.put("timestamp", LocalDateTime.now().toString());
+        errorAttributes.put("requestUrl", ElementsRequestInterceptor.requestUrl);
+
+        return errorAttributes;
+      }
+
+    };
   }
 }
---
+++ src/main/java/org/heigit/bigspatialdata/ohsome/ohsomeApi/exception/NotFoundException.java
@@ -0,0 +1,16 @@
+package org.heigit.bigspatialdata.ohsome.ohsomeApi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/** Exception class corresponding to the HTTP status code 404. */
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException {
+
+  private static final long serialVersionUID = -2152797354661700697L;
+
+  public NotFoundException(String message) {
+    super(message);
+  }
+
+}
