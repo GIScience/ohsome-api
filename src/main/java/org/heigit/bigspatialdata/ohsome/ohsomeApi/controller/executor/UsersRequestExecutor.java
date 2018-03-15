3e48b2ca3c8a6578d94f33276dd486c4a64ee826
kowatsch <kowatsch@heigit4.geog.uni-heidelberg.de>
2018-03-15 15:25:13 +0100

    implementing /user/count

---
+++ src/main/java/org/heigit/bigspatialdata/ohsome/ohsomeApi/controller/executor/UsersRequestExecutor.java
@@ -0,0 +1,80 @@
+package org.heigit.bigspatialdata.ohsome.ohsomeApi.controller.executor;
+
+import java.util.Map.Entry;
+import java.util.SortedMap;
+import org.heigit.bigspatialdata.ohsome.ohsomeApi.Application;
+import org.heigit.bigspatialdata.ohsome.ohsomeApi.exception.BadRequestException;
+import org.heigit.bigspatialdata.ohsome.ohsomeApi.inputProcessing.InputProcessor;
+import org.heigit.bigspatialdata.ohsome.ohsomeApi.interceptor.ElementsRequestInterceptor;
+import org.heigit.bigspatialdata.ohsome.ohsomeApi.output.dataAggregationResponse.Attribution;
+import org.heigit.bigspatialdata.ohsome.ohsomeApi.output.dataAggregationResponse.DefaultAggregationResponse;
+import org.heigit.bigspatialdata.ohsome.ohsomeApi.output.dataAggregationResponse.metadata.Metadata;
+import org.heigit.bigspatialdata.ohsome.ohsomeApi.output.dataAggregationResponse.result.Result;
+import org.heigit.bigspatialdata.oshdb.api.mapreducer.MapReducer;
+import org.heigit.bigspatialdata.oshdb.api.object.OSMContribution;
+import org.heigit.bigspatialdata.oshdb.util.OSHDBTimestamp;
+import org.heigit.bigspatialdata.oshdb.util.time.TimestampFormatter;
+
+public class UsersRequestExecutor {
+
+  private static final String url = Application.getAttributionUrl();
+  private static final String text = Application.getAttributionShort();
+  private static final String apiVersion = "0.9";
+
+  /**
+   * Performs a count calculation.
+   * <p>
+   * The parameters are described in the
+   * {@link org.heigit.bigspatialdata.ohsome.ohsomeApi.controller.elements.CountController#getCount(String, String, String, String[], String[], String[], String[], String[], String)
+   * getCount} method.
+   * 
+   * @return {@link org.heigit.bigspatialdata.ohsome.ohsomeApi.output.dataAggregationResponse.DefaultAggregationResponse
+   *         DefaultAggregationResponse}
+   * @throws UnsupportedOperationException by
+   *         {@link org.heigit.bigspatialdata.oshdb.api.mapreducer.MapReducer#aggregateByTimestamp()
+   *         aggregateByTimestamp()}
+   * @throws BadRequestException by
+   *         {@link org.heigit.bigspatialdata.ohsome.ohsomeApi.inputProcessing.InputProcessor#processParameters(boolean, String, String, String, String[], String[], String[], String[], String[], String)
+   *         processParameters()}
+   * @throws Exception by
+   *         {@link org.heigit.bigspatialdata.oshdb.api.mapreducer.MapAggregator#count() count()}
+   */
+  public static DefaultAggregationResponse executeCount(boolean isPost, String bboxes,
+      String bcircles, String bpolys, String[] types, String[] keys, String[] values,
+      String[] userids, String[] time, String showMetadata)
+      throws UnsupportedOperationException, BadRequestException, Exception {
+
+    long startTime = System.currentTimeMillis();
+    SortedMap<OSHDBTimestamp, Integer> result;
+    MapReducer<OSMContribution> mapRed;
+    InputProcessor iP = new InputProcessor();
+    String requestURL = null;
+
+    if (!isPost)
+      requestURL = ElementsRequestInterceptor.requestUrl;
+    mapRed = iP.processUsersParameters(isPost, bboxes, bcircles, bpolys, types, keys, values,
+        userids, time, showMetadata);
+    // db result
+    result = mapRed.aggregateByTimestamp().map(contrib -> {
+      return contrib.getContributorUserId();
+    }).countUniq();
+    Result[] resultSet = new Result[result.size()];
+    int count = 0;
+
+    for (Entry<OSHDBTimestamp, Integer> entry : result.entrySet()) {
+      resultSet[count] = new Result(TimestampFormatter.getInstance().isoDateTime(entry.getKey()),
+          entry.getValue().intValue());
+      count++;
+    }
+    Metadata metadata = null;
+    long duration = System.currentTimeMillis() - startTime;
+    if (iP.getShowMetadata()) {
+      metadata = new Metadata(duration, "Number of different users.", requestURL);
+    }
+    DefaultAggregationResponse response =
+        new DefaultAggregationResponse(new Attribution(url, text), apiVersion, metadata, resultSet);
+
+    return response;
+  }
+
+}
--- src/main/java/org/heigit/bigspatialdata/ohsome/ohsomeApi/controller/users/UsersController.java
+++ src/main/java/org/heigit/bigspatialdata/ohsome/ohsomeApi/controller/users/UsersController.java
@@ -1,7 +1,13 @@
 package org.heigit.bigspatialdata.ohsome.ohsomeApi.controller.users;
 
+import org.heigit.bigspatialdata.ohsome.ohsomeApi.controller.executor.UsersRequestExecutor;
+import org.heigit.bigspatialdata.ohsome.ohsomeApi.output.dataAggregationResponse.DefaultAggregationResponse;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
 /**
  * REST controller containing the GET and POST requests, which enter through "/users".
@@ -11,6 +17,42 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UsersController {
 
+  /**
+   * GET request giving the count of OSM users.
+   * <p>
+   * The parameters are described in the
+   * {@link org.heigit.bigspatialdata.ohsome.ohsomeApi.controller.elements.CountController#getCount(String, String, String, String[], String[], String[], String[], String[], String)
+   * getCount} method.
+   * 
+   * @return {@link org.heigit.bigspatialdata.ohsome.ohsomeApi.output.dataAggregationResponse.GroupByTypeResponse
+   *         GroupByTypeResponseContent}
+   */
+  @ApiOperation(value = "Count of OSM elements grouped by the type")
+  @RequestMapping(value = "/count", method = RequestMethod.GET,
+      produces = "application/json")
+  public DefaultAggregationResponse getCountGroupByType(
+      @ApiParam(hidden = true) @RequestParam(value = "bboxes", defaultValue = "",
+          required = false) String bboxes,
+      @ApiParam(hidden = true) @RequestParam(value = "bcircles", defaultValue = "",
+          required = false) String bcircles,
+      @ApiParam(hidden = true) @RequestParam(value = "bpolys", defaultValue = "",
+          required = false) String bpolys,
+      @ApiParam(hidden = true) @RequestParam(value = "types", defaultValue = "",
+          required = false) String[] types,
+      @ApiParam(hidden = true) @RequestParam(value = "keys", defaultValue = "",
+          required = false) String[] keys,
+      @ApiParam(hidden = true) @RequestParam(value = "values", defaultValue = "",
+          required = false) String[] values,
+      @ApiParam(hidden = true) @RequestParam(value = "userids", defaultValue = "",
+          required = false) String[] userids,
+      @ApiParam(hidden = true) @RequestParam(value = "time", defaultValue = "",
+          required = false) String[] time,
+      @ApiParam(hidden = true) @RequestParam(value = "showMetadata",
+          defaultValue = "false") String showMetadata)
+      throws UnsupportedOperationException, Exception {
 
+    return UsersRequestExecutor.executeCount(false, bboxes, bcircles, bpolys, types, keys,
+        values, userids, time, showMetadata);
+  }
 
 }
