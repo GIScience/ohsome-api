643ec56cc1d2fdbf84fdd053f014d287d237a344
kowatsch <kowatsch@heigit4.geog.uni-heidelberg.de>
2018-04-20 10:15:28 +0200

    extending enum RequestResource

--- src/main/java/org/heigit/bigspatialdata/ohsome/ohsomeApi/controller/executor/RequestResource.java
+++ src/main/java/org/heigit/bigspatialdata/ohsome/ohsomeApi/controller/executor/RequestResource.java
@@ -1,6 +1,23 @@
 package org.heigit.bigspatialdata.ohsome.ohsomeApi.controller.executor;
 
-/** Enumeration defining the request resource (LENGTH, PERIMETER, AREA) */
+/** Enumeration defining the request resource (LENGTH, PERIMETER, AREA, COUNT) */
 public enum RequestResource {
-  LENGTH, PERIMETER, AREA
+  LENGTH("length", "meters"), PERIMETER("perimeter", "meters"), AREA("area",
+      "square meters"), COUNT("count", "amount");
+
+  private final String label;
+  private final String unit;
+
+  RequestResource(String label, String unit) {
+    this.label = label;
+    this.unit = unit;
+  }
+
+  public String getLabel() {
+    return label;
+  }
+
+  public String getUnit() {
+    return unit;
+  }
 }
