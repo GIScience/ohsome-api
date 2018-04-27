5ea13d8bf4d1f504a7c5df6434cea53e204bd9b8
kowatsch <kowatsch@heigit4.geog.uni-heidelberg.de>
2018-04-27 15:08:58 +0200

    implementing users/density

--- src/main/java/org/heigit/bigspatialdata/ohsome/ohsomeApi/controller/dataAggregation/UsersController.java
+++ src/main/java/org/heigit/bigspatialdata/ohsome/ohsomeApi/controller/dataAggregation/UsersController.java
@@ -105,4 +105,88 @@ public class UsersController {
         bcircles, bpolys, types, keys, values, userids, time, showMetadata));
   }
 
+  /**
+   * GET request giving the density of OSM users (number of users per square-kilometers).
+   * <p>
+   * The parameters are described in the
+   * {@link org.heigit.bigspatialdata.ohsome.ohsomeApi.controller.dataAggregation.CountController#getCount(String, String, String, String[], String[], String[], String[], String[], String)
+   * getCount} method.
+   * 
+   * @return {@link org.heigit.bigspatialdata.ohsome.ohsomeApi.output.dataAggregationResponse.elements.DefaultAggregationResponse
+   *         DefaultAggregationResponse}
+   */
+  @ApiOperation(value = "Density of OSM users (number of users per square-kilometers)")
+  @RequestMapping(value = "/count/density", method = RequestMethod.GET, produces = "application/json")
+  public DefaultUsersResponse getCountDensity(
+      @ApiParam(hidden = true) @RequestParam(value = "bboxes", defaultValue = "",
+          required = false) String bboxes,
+      @ApiParam(hidden = true) @RequestParam(value = "bcircles", defaultValue = "",
+          required = false) String bcircles,
+      @ApiParam(hidden = true) @RequestParam(value = "bpolys", defaultValue = "",
+          required = false) String bpolys,
+      @ApiParam(hidden = true) @RequestParam(value = "types", defaultValue = "",
+          required = false) String[] types,
+      @ApiParam(hidden = true) @RequestParam(value = "keys", defaultValue = "",
+          required = false) String[] keys,
+      @ApiParam(hidden = true) @RequestParam(value = "values", defaultValue = "",
+          required = false) String[] values,
+      @ApiParam(hidden = true) @RequestParam(value = "userids", defaultValue = "",
+          required = false) String[] userids,
+      @ApiParam(hidden = true) @RequestParam(value = "time", defaultValue = "",
+          required = false) String[] time,
+      @ApiParam(hidden = true) @RequestParam(value = "showMetadata",
+          defaultValue = "false") String showMetadata)
+      throws UnsupportedOperationException, Exception {
+
+    return UsersRequestExecutor.executeCount(new RequestParameters(false, false, true, bboxes,
+        bcircles, bpolys, types, keys, values, userids, time, showMetadata));
+  }
+
+  /**
+   * POST request giving the density of OSM users (number of users per square-kilometers). POST
+   * requests should only be used if the request URL would be too long for a GET request.
+   * <p>
+   * The parameters are described in the
+   * {@link org.heigit.bigspatialdata.ohsome.ohsomeApi.controller.dataAggregation.CountController#getCount(String, String, String, String[], String[], String[], String[], String[], String)
+   * getCount} method.
+   * 
+   * @return {@link org.heigit.bigspatialdata.ohsome.ohsomeApi.output.dataAggregationResponse.elements.DefaultAggregationResponse
+   *         DefaultAggregationResponse}
+   */
+  @ApiOperation(value = "Density of OSM users (number of users per square-kilometers)")
+  @ApiImplicitParams({
+      @ApiImplicitParam(name = "bboxes", paramType = "form", dataType = "string",
+          defaultValue = DefaultSwaggerParameters.BBOX, required = false,
+          value = ParameterDescriptions.BBOXES_DESCR),
+      @ApiImplicitParam(name = "bcircles", paramType = "form", dataType = "string",
+          required = false, value = ParameterDescriptions.BCIRCLES_DESCR),
+      @ApiImplicitParam(name = "bpolys", paramType = "form", dataType = "string", required = false,
+          value = ParameterDescriptions.BPOLYS_DESCR),
+      @ApiImplicitParam(name = "types", paramType = "form", dataType = "string",
+          defaultValue = DefaultSwaggerParameters.TYPE, required = false,
+          value = ParameterDescriptions.TYPES_DESCR),
+      @ApiImplicitParam(name = "keys", paramType = "form", dataType = "string",
+          defaultValue = DefaultSwaggerParameters.BUILDING_KEY, required = false,
+          value = ParameterDescriptions.KEYS_DESCR),
+      @ApiImplicitParam(name = "values", paramType = "form", dataType = "string",
+          defaultValue = DefaultSwaggerParameters.RESIDENTIAL_VALUE, required = false,
+          value = ParameterDescriptions.VALUES_DESCR),
+      @ApiImplicitParam(name = "userids", paramType = "form", dataType = "string", required = false,
+          value = ParameterDescriptions.USERIDS_DESCR),
+      @ApiImplicitParam(name = "time", paramType = "form", dataType = "string",
+          defaultValue = DefaultSwaggerParameters.TIME, required = false,
+          value = ParameterDescriptions.TIME_DESCR),
+      @ApiImplicitParam(name = "showMetadata", paramType = "form", dataType = "string",
+          defaultValue = DefaultSwaggerParameters.SHOW_METADATA, required = false,
+          value = ParameterDescriptions.SHOW_METADATA_DESCR)})
+  @RequestMapping(value = "/count/density", method = RequestMethod.POST, produces = "application/json",
+      consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+  public DefaultUsersResponse postCountDensity(String bboxes, String bcircles, String bpolys,
+      String[] types, String[] keys, String[] values, String[] userids, String[] time,
+      String showMetadata) throws UnsupportedOperationException, Exception {
+
+    return UsersRequestExecutor.executeCount(new RequestParameters(true, false, true, bboxes,
+        bcircles, bpolys, types, keys, values, userids, time, showMetadata));
+  }
+
 }
--- src/main/java/org/heigit/bigspatialdata/ohsome/ohsomeApi/controller/executor/UsersRequestExecutor.java
+++ src/main/java/org/heigit/bigspatialdata/ohsome/ohsomeApi/controller/executor/UsersRequestExecutor.java
@@ -1,8 +1,10 @@
 package org.heigit.bigspatialdata.ohsome.ohsomeApi.controller.executor;
 
+import java.text.DecimalFormat;
 import java.util.Map.Entry;
 import java.util.SortedMap;
 import org.heigit.bigspatialdata.ohsome.ohsomeApi.Application;
+import org.heigit.bigspatialdata.ohsome.ohsomeApi.inputProcessing.GeometryBuilder;
 import org.heigit.bigspatialdata.ohsome.ohsomeApi.inputProcessing.InputProcessor;
 import org.heigit.bigspatialdata.ohsome.ohsomeApi.interceptor.RequestInterceptor;
 import org.heigit.bigspatialdata.ohsome.ohsomeApi.output.dataAggregationResponse.Attribution;
@@ -12,7 +14,9 @@ import org.heigit.bigspatialdata.ohsome.ohsomeApi.output.dataAggregationResponse
 import org.heigit.bigspatialdata.oshdb.api.mapreducer.MapReducer;
 import org.heigit.bigspatialdata.oshdb.api.object.OSMContribution;
 import org.heigit.bigspatialdata.oshdb.util.OSHDBTimestamp;
+import org.heigit.bigspatialdata.oshdb.util.geometry.Geo;
 import org.heigit.bigspatialdata.oshdb.util.time.TimestampFormatter;
+import com.vividsolutions.jts.geom.Geometry;
 
 /** Includes the execute methods for requests mapped to /users. */
 public class UsersRequestExecutor {
@@ -37,7 +41,10 @@ public class UsersRequestExecutor {
     SortedMap<OSHDBTimestamp, Integer> result;
     MapReducer<OSMContribution> mapRed = null;
     InputProcessor iP = new InputProcessor();
+    ExecutionUtils exeUtils = new ExecutionUtils();
+    String description = null;
     String requestURL = null;
+    DecimalFormat df = exeUtils.defineDecimalFormat("#.##");
     if (!rPs.isPost())
       requestURL = RequestInterceptor.requestUrl;
     mapRed = iP.processParameters(mapRed, rPs);
@@ -47,17 +54,32 @@ public class UsersRequestExecutor {
     }).countUniq();
     UsersResult[] resultSet = new UsersResult[result.size()];
     String[] toTimestamps = iP.getUtils().getToTimestamps();
+    GeometryBuilder geomBuilder = iP.getGeomBuilder();
+    Geometry geom = exeUtils.getGeometry(iP.getBoundaryType(), geomBuilder);
     int count = 0;
     for (Entry<OSHDBTimestamp, Integer> entry : result.entrySet()) {
-      resultSet[count] =
-          new UsersResult(TimestampFormatter.getInstance().isoDateTime(entry.getKey()),
-              toTimestamps[count + 1], entry.getValue().intValue());
+      if (rPs.isDensity()) {
+        resultSet[count] =
+            new UsersResult(TimestampFormatter.getInstance().isoDateTime(entry.getKey()),
+                toTimestamps[count + 1], Double.parseDouble(
+                    df.format((entry.getValue().doubleValue() / (Geo.areaOf(geom) * 0.000001)))));
+      } else {
+        resultSet[count] =
+            new UsersResult(TimestampFormatter.getInstance().isoDateTime(entry.getKey()),
+                toTimestamps[count + 1], entry.getValue().intValue());
+      }
       count++;
+    }
+    if (rPs.isDensity()) {
+      description =
+          "Density of distinct users per time interval (number of users per square-kilometer).";
+    } else {
+      description = "Number of distinct users per time interval.";
     }
     Metadata metadata = null;
     long duration = System.currentTimeMillis() - startTime;
     if (iP.getShowMetadata()) {
-      metadata = new Metadata(duration, "Number of distinct users per time interval.", requestURL);
+      metadata = new Metadata(duration, description, requestURL);
     }
     DefaultUsersResponse response = new DefaultUsersResponse(new Attribution(url, text),
         Application.apiVersion, metadata, resultSet);
