358c0b3c942899e749a208417f99a6f71ddacc3c
kowatsch <kowatsch@heigit4.geog.uni-heidelberg.de>
2018-05-07 12:19:07 +0200

    implementing /users/count/groupBy/tag

--- src/main/java/org/heigit/bigspatialdata/ohsome/ohsomeApi/controller/dataAggregation/UsersController.java
+++ src/main/java/org/heigit/bigspatialdata/ohsome/ohsomeApi/controller/dataAggregation/UsersController.java
@@ -4,6 +4,7 @@ import org.heigit.bigspatialdata.ohsome.ohsomeApi.controller.DefaultSwaggerParam
 import org.heigit.bigspatialdata.ohsome.ohsomeApi.controller.ParameterDescriptions;
 import org.heigit.bigspatialdata.ohsome.ohsomeApi.controller.executor.RequestParameters;
 import org.heigit.bigspatialdata.ohsome.ohsomeApi.controller.executor.UsersRequestExecutor;
+import org.heigit.bigspatialdata.ohsome.ohsomeApi.exception.BadRequestException;
 import org.heigit.bigspatialdata.ohsome.ohsomeApi.output.dataAggregationResponse.DefaultAggregationResponse;
 import org.heigit.bigspatialdata.ohsome.ohsomeApi.output.dataAggregationResponse.groupByResponse.GroupByResponse;
 import org.springframework.http.MediaType;
@@ -96,6 +97,54 @@ public class UsersController {
 
     return UsersRequestExecutor.executeCountGroupByType(new RequestParameters(false, false, false,
         bboxes, bcircles, bpolys, types, keys, values, userids, time, showMetadata));
+  }
+
+  /**
+   * GET request giving the count of OSM users grouped by the OSM type.
+   * <p>
+   * The parameters are described in the
+   * {@link org.heigit.bigspatialdata.ohsome.ohsomeApi.controller.dataAggregation.CountController#getCount(String, String, String, String[], String[], String[], String[], String[], String)
+   * getCount} method.
+   * 
+   * @return {@link org.heigit.bigspatialdata.ohsome.ohsomeApi.output.dataAggregationResponse.groupByResponse.GroupByResponse
+   *         GroupByResponseContent}
+   */
+  @ApiOperation(value = "Count of OSM elements grouped by the tag")
+  @ApiImplicitParams({
+      @ApiImplicitParam(name = "groupByKey", value = ParameterDescriptions.KEYS_DESCR,
+          defaultValue = DefaultSwaggerParameters.BUILDING_KEY, paramType = "query",
+          dataType = "string", required = true),
+      @ApiImplicitParam(name = "groupByValues", value = ParameterDescriptions.VALUES_DESCR,
+          defaultValue = "", paramType = "query", dataType = "string", required = false)})
+  @RequestMapping(value = "count/groupBy/tag", method = RequestMethod.GET,
+      produces = "application/json")
+  public GroupByResponse getCountGroupByTag(
+      @ApiParam(hidden = true) @RequestParam(value = "bboxes", defaultValue = "",
+          required = false) String bboxes,
+      @ApiParam(hidden = true) @RequestParam(value = "bcircles", defaultValue = "",
+          required = false) String bcircles,
+      @ApiParam(hidden = true) @RequestParam(value = "bpolys", defaultValue = "",
+          required = false) String bpolys,
+      @ApiParam(hidden = true) @RequestParam(value = "types", defaultValue = "",
+          required = false) String[] types,
+      @ApiParam(hidden = true) @RequestParam(value = "keys", defaultValue = "",
+          required = false) String[] keys,
+      @ApiParam(hidden = true) @RequestParam(value = "values", defaultValue = "",
+          required = false) String[] values,
+      @ApiParam(hidden = true) @RequestParam(value = "userids", defaultValue = "",
+          required = false) String[] userids,
+      @ApiParam(hidden = true) @RequestParam(value = "time", defaultValue = "",
+          required = false) String[] time,
+      @ApiParam(hidden = true) @RequestParam(value = "showMetadata",
+          defaultValue = "false") String showMetadata,
+      @RequestParam(value = "groupByKey", defaultValue = "", required = false) String[] groupByKey,
+      @RequestParam(value = "groupByValues", defaultValue = "",
+          required = false) String[] groupByValues)
+      throws UnsupportedOperationException, Exception {
+
+    return UsersRequestExecutor.executeCountGroupByTag(new RequestParameters(false, false, false,
+        bboxes, bcircles, bpolys, types, keys, values, userids, time, showMetadata), groupByKey,
+        groupByValues);
   }
 
   /**
@@ -264,6 +313,64 @@ public class UsersController {
 
     return UsersRequestExecutor.executeCountGroupByType(new RequestParameters(true, false, false,
         bboxes, bcircles, bpolys, types, keys, values, userids, time, showMetadata));
+  }
+
+  /**
+   * POST request giving the count of OSM users grouped by the tag. POST requests should only be
+   * used if the request URL would be too long for a GET request.
+   * <p>
+   * The other parameters are described in the
+   * {@link org.heigit.bigspatialdata.ohsome.ohsomeApi.controller.dataAggregation.CountController#getCount(String, String, String, String[], String[], String[], String[], String[], String)
+   * getCount} method.
+   * 
+   * @param groupByKey <code>String</code> array containing the key used to create the tags for the
+   *        grouping. At the current implementation, there must be one key given (not more and not
+   *        less).
+   * @param groupByValues <code>String</code> array containing the values used to create the tags
+   *        for grouping. If a given value does not appear in the output, then there are no objects
+   *        assigned to it (within the given filters).
+   * @return {@link org.heigit.bigspatialdata.ohsome.ohsomeApi.output.dataAggregationResponse.groupByResponse.GroupByResponse
+   *         GroupByResponse Content}
+   */
+  @ApiOperation(value = "Count of OSM users grouped by the tag")
+  @ApiImplicitParams({
+      @ApiImplicitParam(name = "bboxes", paramType = "form", dataType = "string",
+          defaultValue = DefaultSwaggerParameters.BBOX, required = false,
+          value = ParameterDescriptions.BBOXES_DESCR),
+      @ApiImplicitParam(name = "bcircles", paramType = "form", dataType = "string",
+          required = false, value = ParameterDescriptions.BCIRCLES_DESCR),
+      @ApiImplicitParam(name = "bpolys", paramType = "form", dataType = "string", required = false,
+          value = ParameterDescriptions.BPOLYS_DESCR),
+      @ApiImplicitParam(name = "types", paramType = "form", dataType = "string",
+          defaultValue = DefaultSwaggerParameters.TYPE, required = false,
+          value = ParameterDescriptions.TYPES_DESCR),
+      @ApiImplicitParam(name = "keys", paramType = "form", dataType = "string",
+          defaultValue = DefaultSwaggerParameters.BUILDING_KEY, required = false,
+          value = ParameterDescriptions.KEYS_DESCR),
+      @ApiImplicitParam(name = "values", paramType = "form", dataType = "string", defaultValue = "",
+          required = false, value = ParameterDescriptions.VALUES_DESCR),
+      @ApiImplicitParam(name = "userids", paramType = "form", dataType = "string", required = false,
+          value = ParameterDescriptions.USERIDS_DESCR),
+      @ApiImplicitParam(name = "time", paramType = "form", dataType = "string",
+          defaultValue = DefaultSwaggerParameters.TIME, required = false,
+          value = ParameterDescriptions.TIME_DESCR),
+      @ApiImplicitParam(name = "showMetadata", paramType = "form", dataType = "string",
+          defaultValue = DefaultSwaggerParameters.SHOW_METADATA, required = false,
+          value = ParameterDescriptions.SHOW_METADATA_DESCR),
+      @ApiImplicitParam(name = "groupByKey", paramType = "form", dataType = "string",
+          defaultValue = "height", required = true, value = ParameterDescriptions.KEYS_DESCR),
+      @ApiImplicitParam(name = "groupByValues", paramType = "form", dataType = "string",
+          defaultValue = "", required = false, value = ParameterDescriptions.VALUES_DESCR)})
+  @RequestMapping(value = "/count/groupBy/tag", method = RequestMethod.POST,
+      produces = "application/json", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+  public GroupByResponse postCountGroupByTag(String bboxes, String bcircles, String bpolys,
+      String[] types, String[] keys, String[] values, String[] userids, String[] time,
+      String showMetadata, String[] groupByKey, String[] groupByValues)
+      throws UnsupportedOperationException, Exception, BadRequestException {
+
+    return UsersRequestExecutor.executeCountGroupByTag(new RequestParameters(true, false, false,
+        bboxes, bcircles, bpolys, types, keys, values, userids, time, showMetadata), groupByKey,
+        groupByValues);
   }
 
   /**
--- src/main/java/org/heigit/bigspatialdata/ohsome/ohsomeApi/controller/executor/UsersRequestExecutor.java
+++ src/main/java/org/heigit/bigspatialdata/ohsome/ohsomeApi/controller/executor/UsersRequestExecutor.java
@@ -1,12 +1,17 @@
 package org.heigit.bigspatialdata.ohsome.ohsomeApi.controller.executor;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.Map.Entry;
 import java.util.SortedMap;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 import org.heigit.bigspatialdata.ohsome.ohsomeApi.Application;
+import org.heigit.bigspatialdata.ohsome.ohsomeApi.exception.BadRequestException;
 import org.heigit.bigspatialdata.ohsome.ohsomeApi.inputProcessing.GeometryBuilder;
 import org.heigit.bigspatialdata.ohsome.ohsomeApi.inputProcessing.InputProcessor;
 import org.heigit.bigspatialdata.ohsome.ohsomeApi.interceptor.RequestInterceptor;
+import org.heigit.bigspatialdata.ohsome.ohsomeApi.oshdb.DbConnData;
 import org.heigit.bigspatialdata.ohsome.ohsomeApi.oshdb.ExtractMetadata;
 import org.heigit.bigspatialdata.ohsome.ohsomeApi.output.dataAggregationResponse.Attribution;
 import org.heigit.bigspatialdata.ohsome.ohsomeApi.output.dataAggregationResponse.DefaultAggregationResponse;
@@ -22,6 +27,7 @@ import org.heigit.bigspatialdata.oshdb.api.object.OSMContribution;
 import org.heigit.bigspatialdata.oshdb.osm.OSMType;
 import org.heigit.bigspatialdata.oshdb.util.OSHDBTimestamp;
 import org.heigit.bigspatialdata.oshdb.util.geometry.Geo;
+import org.heigit.bigspatialdata.oshdb.util.tagtranslator.TagTranslator;
 import org.heigit.bigspatialdata.oshdb.util.time.TimestampFormatter;
 import com.vividsolutions.jts.geom.Geometry;
 
@@ -166,5 +172,118 @@ public class UsersRequestExecutor {
         Application.apiVersion, metadata, resultSet);
     return response;
   }
+
+  /**
+   * Performs a count calculation grouped by the tag.
+   * <p>
+   * The other parameters are described in the
+   * {@link org.heigit.bigspatialdata.ohsome.ohsomeApi.controller.dataAggregation.CountController#getCountGroupByTag(String, String, String, String[], String[], String[], String[], String[], String, String[], String[])
+   * getCountGroupByTag} method.
+   * 
+   * @param rPs <code>RequestParameters</code> object, which holds those parameters that are used in
+   *        every request.
+   * @return {@link org.heigit.bigspatialdata.ohsome.ohsomeApi.output.dataAggregationResponse.groupByResponse.GroupByResponse
+   *         GroupByResponseContent}
+   */
+  public static GroupByResponse executeCountGroupByTag(RequestParameters rPs, String[] groupByKey,
+      String[] groupByValues) throws UnsupportedOperationException, Exception {
+
+    long startTime = System.currentTimeMillis();
+    if (groupByKey == null || groupByKey.length != 1)
+      throw new BadRequestException(
+          "You need to give one groupByKey parameter, if you want to use groupBy/tag.");
+    ExecutionUtils exeUtils = new ExecutionUtils();
+    SortedMap<OSHDBTimestampAndIndex<Pair<Integer, Integer>>, Integer> result = null;
+    SortedMap<Pair<Integer, Integer>, SortedMap<OSHDBTimestamp, Integer>> groupByResult;
+    MapReducer<OSMContribution> mapRed = null;
+    InputProcessor iP = new InputProcessor();
+    String description = null;
+    String requestURL = null;
+    DecimalFormat df = exeUtils.defineDecimalFormat("#.##");
+    if (!rPs.isPost())
+      requestURL = RequestInterceptor.requestUrl;
+    if (groupByValues == null)
+      groupByValues = new String[0];
+    TagTranslator tt = DbConnData.tagTranslator;
+    Integer[] valuesInt = new Integer[groupByValues.length];
+    ArrayList<Pair<Integer, Integer>> zeroFill = new ArrayList<Pair<Integer, Integer>>();
+    int keysInt = tt.getOSHDBTagKeyOf(groupByKey[0]).toInt();
+    if (groupByValues.length != 0) {
+      for (int j = 0; j < groupByValues.length; j++) {
+        valuesInt[j] = tt.getOSHDBTagOf(groupByKey[0], groupByValues[j]).getValue();
+        zeroFill.add(new ImmutablePair<Integer, Integer>(keysInt, valuesInt[j]));
+      }
+    }
+    mapRed = iP.processParameters(mapRed, rPs);
+    result = mapRed.map(f -> {
+      int[] tags = f.getEntityAfter().getRawTags();
+      for (int i = 0; i < tags.length; i += 2) {
+        int tagKeyId = tags[i];
+        int tagValueId = tags[i + 1];
+        if (tagKeyId == keysInt) {
+          if (valuesInt.length == 0) {
+            return new ImmutablePair<>(new ImmutablePair<Integer, Integer>(tagKeyId, tagValueId),
+                f);
+          }
+          for (int value : valuesInt) {
+            if (tagValueId == value)
+              return new ImmutablePair<>(new ImmutablePair<Integer, Integer>(tagKeyId, tagValueId),
+                  f);
+          }
+        }
+      }
+      return new ImmutablePair<>(new ImmutablePair<Integer, Integer>(-1, -1), f);
+    }).aggregateByTimestamp().aggregateBy(Pair::getKey).zerofillIndices(zeroFill)
+        .map(Pair::getValue).countUniq();
+
+    groupByResult = MapAggregatorByTimestampAndIndex.nest_IndexThenTime(result);
+    GroupByResult[] resultSet = new GroupByResult[groupByResult.size()];
+    String groupByName = "";
+    GeometryBuilder geomBuilder = iP.getGeomBuilder();
+    Geometry geom = exeUtils.getGeometry(iP.getBoundaryType(), geomBuilder);
+    String[] toTimestamps = iP.getUtils().getToTimestamps();
+    int count = 0;
+    for (Entry<Pair<Integer, Integer>, SortedMap<OSHDBTimestamp, Integer>> entry : groupByResult
+        .entrySet()) {
+      UsersResult[] results = new UsersResult[entry.getValue().entrySet().size()];
+      int innerCount = 0;
+      // check for non-remainder objects (which do have the defined key and value)
+      if (entry.getKey().getKey() != -1 && entry.getKey().getValue() != -1) {
+        groupByName = tt.getOSMTagOf(keysInt, entry.getKey().getValue()).toString();
+      } else {
+        groupByName = "remainder";
+      }
+      for (Entry<OSHDBTimestamp, ? extends Number> innerEntry : entry.getValue().entrySet()) {
+        if (rPs.isDensity())
+          results[innerCount] = new UsersResult(
+              TimestampFormatter.getInstance().isoDateTime(innerEntry.getKey()),
+              toTimestamps[innerCount + 1], Double.parseDouble(
+                  df.format((innerEntry.getValue().doubleValue() / (Geo.areaOf(geom) / 1000000)))));
+        else
+          results[innerCount] =
+              new UsersResult(TimestampFormatter.getInstance().isoDateTime(innerEntry.getKey()),
+                  toTimestamps[innerCount + 1],
+                  Double.parseDouble(df.format(innerEntry.getValue().doubleValue())));
+        innerCount++;
+      }
+      resultSet[count] = new GroupByResult(groupByName, results);
+      count++;
+    }
+    if (rPs.isDensity()) {
+      description =
+          "Density of distinct users per time interval (number of users per square-kilometer) aggregated on the tag.";
+    } else {
+      description = "Number of distinct users per time interval aggregated on the tag.";
+    }
+    Metadata metadata = null;
+    if (iP.getShowMetadata()) {
+      long duration = System.currentTimeMillis() - startTime;
+      metadata = new Metadata(duration, description, requestURL);
+    }
+    GroupByResponse response = new GroupByResponse(new Attribution(url, text),
+        Application.apiVersion, metadata, resultSet);
+    return response;
+  }
+
 
 }
