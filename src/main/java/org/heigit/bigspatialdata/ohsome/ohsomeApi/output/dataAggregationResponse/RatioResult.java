91ee708b1efedfe97db456332aa1c8e86231d532
kowatsch <kowatsch@heigit4.geog.uni-heidelberg.de>
2018-08-13 11:50:06 +0200

    extending interface 'Result' usage

--- src/main/java/org/heigit/bigspatialdata/ohsome/ohsomeApi/output/dataAggregationResponse/RatioResult.java
+++ src/main/java/org/heigit/bigspatialdata/ohsome/ohsomeApi/output/dataAggregationResponse/RatioResult.java
@@ -6,7 +6,7 @@ import io.swagger.annotations.ApiModelProperty;
  * Represents the result JSON object for the /ratio resource containing the timestamp together with
  * two values and their resulting ratio.
  */
-public class RatioResult {
+public class RatioResult implements Result {
 
   @ApiModelProperty(notes = "Timestamp in the format YYYY-MM-DDThh:mm:ssZ", required = true)
   private String timestamp;
