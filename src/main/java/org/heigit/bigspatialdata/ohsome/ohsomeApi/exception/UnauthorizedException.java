10ab72489a5f1d0db68dc6e84ece1d016b7179e7
kowatsch <kowatsch@heigit4.geog.uni-heidelberg.de>
2018-08-13 13:21:49 +0200

    adapting exception extension
    so all custom exception classes extend RuntimeException

--- src/main/java/org/heigit/bigspatialdata/ohsome/ohsomeApi/exception/UnauthorizedException.java
+++ src/main/java/org/heigit/bigspatialdata/ohsome/ohsomeApi/exception/UnauthorizedException.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 /** Exception class corresponding to the HTTP status code 401. */
 @ResponseStatus(value = HttpStatus.UNAUTHORIZED)
-public class UnauthorizedException extends Exception {
+public class UnauthorizedException extends RuntimeException {
 
   private static final long serialVersionUID = -3021015133062599721L;
 
