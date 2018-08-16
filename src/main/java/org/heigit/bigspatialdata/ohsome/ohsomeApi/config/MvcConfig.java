62ddf6479fda6845099377dd6f95f431520f899f
kowatsch <kowatsch@heigit4.geog.uni-heidelberg.de>
2018-08-16 09:30:57 +0200

    fixing swagger display
    @EnableWebMvc annotation was overwriting swagger config

--- src/main/java/org/heigit/bigspatialdata/ohsome/ohsomeApi/config/MvcConfig.java
+++ src/main/java/org/heigit/bigspatialdata/ohsome/ohsomeApi/config/MvcConfig.java
@@ -3,7 +3,6 @@ package org.heigit.bigspatialdata.ohsome.ohsomeApi.config;
 import org.heigit.bigspatialdata.ohsome.ohsomeApi.interceptor.RequestInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,7 +11,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * RequestInterceptor} class into the spring MVC life cycle.
  */
 @Configuration
-@EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer {
 
   @Autowired
