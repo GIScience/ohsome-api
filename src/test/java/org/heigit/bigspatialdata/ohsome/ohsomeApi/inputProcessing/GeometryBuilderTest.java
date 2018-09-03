36f292745ef518fa33425d710353b1a3369b2c9d
kowatsch <fabian.kowatsch@uni-heidelberg.de>
2018-09-03 17:33:45 +0200

    modifying GeometryBuilderTest cases
    to use data from Heidelberg.oshdb file

--- src/test/java/org/heigit/bigspatialdata/ohsome/ohsomeApi/inputProcessing/GeometryBuilderTest.java
+++ src/test/java/org/heigit/bigspatialdata/ohsome/ohsomeApi/inputProcessing/GeometryBuilderTest.java
@@ -28,26 +28,23 @@ public class GeometryBuilderTest {
   @Test(expected = BadRequestException.class)
   public void createPolygonWithWrongCoordinatesFromBboxes() {
 
-    String[] bcircles = new String[] {"85.21987", "27.7679", "85.4079", "27.8649", "85.3022",
-        "27.7084", "[invalid input]", "27.8115"};
-    geomBuilder.createBboxes(bcircles);
+    String[] coords = new String[] {"8.67452", "49.40961", "8.70392", "[invalid input]", "8.68302", "49.41044", "8.69722", "49.41639"};
+    geomBuilder.createBboxes(coords);
   }
 
   @Test
   public void createOverlappingPolygonFromBboxes() {
 
-    String[] bcircles = new String[] {"85.21987", "27.7679", "85.4079", "27.8649", "85.3022",
-        "27.7084", "85.4422", "27.8115"};
-    Geometry geom = geomBuilder.createBboxes(bcircles);
+    String[] coords = new String[] {"8.678", "49.4148", "8.68864", "49.41971", "8.68302", "49.41044", "8.69722", "49.41639"};
+    Geometry geom = geomBuilder.createBboxes(coords);
     assertTrue(geom instanceof Polygon);
   }
 
   @Test
   public void createNonOverlappingMultiPolygonFromBboxes() {
 
-    String[] bcircles = new String[] {"85.3118", "27.7472", "85.4312", "27.8334", "85.1855",
-        "27.6538", "85.2841", "27.7771"};
-    Geometry geom = geomBuilder.createBboxes(bcircles);
+    String[] coords = new String[] {"8.67976", "49.41767", "8.68817", "49.42214", "8.6974", "49.40882", "8.70722", "49.41276"};
+    Geometry geom = geomBuilder.createBboxes(coords);
     assertTrue(geom instanceof MultiPolygon);
   }
 
@@ -57,14 +54,14 @@ public class GeometryBuilderTest {
   public void createPolygonWithWrongCoordinatesFromBcircles() {
 
     String[] bcircles =
-        new String[] {"85.3653", "27.8154", "[invalid input]", "85.1704", "27.7706", "8056"};
+        new String[] {"8.68452", "49.41781", "[invalid input]", "8.68491", "49.4179", "117"};
     geomBuilder.createCircularPolygons(bcircles);
   }
 
   @Test
   public void createOverlappingPolygonFromBcircles() {
 
-    String[] bcircles = new String[] {"85.3431", "27.7582", "6762", "85.2361", "27.6684", "11104"};
+    String[] bcircles = new String[] {"8.68452", "49.41781", "100", "8.68491", "49.4179", "100"};
     Geometry geom = geomBuilder.createCircularPolygons(bcircles);
     assertTrue(geom instanceof Polygon);
   }
@@ -72,7 +69,7 @@ public class GeometryBuilderTest {
   @Test
   public void createNonOverlappingMultiPolygonFromBcircles() {
 
-    String[] bcircles = new String[] {"85.3653", "27.8154", "5239", "85.1704", "27.7706", "8056"};
+    String[] bcircles = new String[] {"8.68242", "49.42482", "100", "8.68692", "49.40449", "100"};
     Geometry geom = geomBuilder.createCircularPolygons(bcircles);
     assertTrue(geom instanceof MultiPolygon);
     assertEquals(geom.getNumGeometries(), bcircles.length / 3);
@@ -84,9 +81,7 @@ public class GeometryBuilderTest {
   public void createPolygonWithWrongCoordinatesFromBpolys() {
 
     String[] bpolys =
-        new String[] {"85.2712", "27.6122", "85.5374", "27.6219", "85.5100", "27.6899", "85.3151",
-            "27.7117", "85.2712", "[invalid input]", "85.2163", "27.6753", "85.3865", "27.6559",
-            "85.4798", "27.7433", "85.4263", "27.8197", "85.2492", "27.7906", "85.2163", "27.6753"};
+        new String[] {"8.6974", "49.40882", "8.6974", "49.41276", "8.70722", "49.41276", "8.70722", "[invalid input]"};
     geomBuilder.createBpolys(bpolys);
   }
 
@@ -94,9 +89,8 @@ public class GeometryBuilderTest {
   public void createOverlappingPolygonFromBpolys() {
 
     String[] bpolys =
-        new String[] {"85.2712", "27.6122", "85.5374", "27.6219", "85.5100", "27.6899", "85.3151",
-            "27.7117", "85.2712", "27.6122", "85.2163", "27.6753", "85.3865", "27.6559", "85.4798",
-            "27.7433", "85.4263", "27.8197", "85.2492", "27.7906", "85.2163", "27.6753"};
+        new String[] {"8.678", "49.4148", "8.678", "49.41971", "8.68864", "49.41971", "8.68864", "49.4148", "8.678", "49.4148", 
+            "8.678", "49.4147", "8.678", "49.41972", "8.68864", "49.41972", "8.68864", "49.4147", "8.678", "49.4147"};
     Geometry geom = geomBuilder.createBpolys(bpolys);
     assertTrue(geom instanceof Polygon);
     assertTrue(((Polygon) geom).getExteriorRing().getNumPoints() < bpolys.length / 2);
@@ -105,9 +99,8 @@ public class GeometryBuilderTest {
   @Test
   public void createNonOverlappingMultiPolygonFromBpolys() {
 
-    String[] bpolys = new String[] {"85.2575", "27.5502", "85.3892", "27.5708", "85.3302",
-        "27.6097", "85.2575", "27.5502", "85.2575", "27.7190", "85.3906", "27.66201", "85.2547",
-        "27.6656", "85.2575", "27.7190"};
+    String[] bpolys = new String[] {"8.678", "49.4148", "8.678", "49.41971", "8.68864", "49.41971", "8.678", "49.4148", 
+        "8.67229", "49.40343", "8.67229", "49.40804", "8.67941", "49.40804", "8.67229", "49.40343"};
     Geometry geom = geomBuilder.createBpolys(bpolys);
     assertTrue(geom instanceof MultiPolygon);
     MultiPolygon mp = (MultiPolygon) geom;
@@ -122,7 +115,7 @@ public class GeometryBuilderTest {
   public void createPolygonFromGeoJsonWithWrongGeomType() {
 
     String geoJson =
-        "{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"properties\":{\"id\":\"Kathmandu\"},\"geometry\":{\"type\":\"Point\",\"coordinates\":[[[85.2044677734375,27.907058371121995]]]}}]}";
+        "{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"properties\":{\"id\":\"Neuenheim\"},\"geometry\":{\"type\":\"Point\",\"coordinates\":[[[8.68465,49.41769]]]}}]}";
     InputProcessor iP = new InputProcessor();
     geomBuilder.createGeometryFromGeoJson(geoJson, iP);
   }
@@ -131,10 +124,10 @@ public class GeometryBuilderTest {
   public void createPolygonFromGeoJson() {
 
     String geoJson =
-        "{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"properties\":{\"id\":\"Kathmandu\"},\"geometry\":{\"type\":\"Polygon\","
-            + "\"coordinates\":[[[85.20447,27.907059],[84.94629,27.63974],[85.39673,27.420538],[85.6604,27.766191],[85.20447,27.907059]]]}},"
-            + "{\"type\":\"Feature\",\"properties\":{\"id\":\"Pokhara\"},\"geometry\":{\"type\":\"Polygon\","
-            + "\"coordinates\":[[[83.913574,28.343065],[83.78723,28.17856],[84.0564,27.974998],[84.28162,28.246328],[83.913574,28.343065]]]}}]}";
+        "{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"properties\":{\"id\":\"Neuenheim\"},\"geometry\":{\"type\":\"Polygon\","
+            + "\"coordinates\":[[[8.65821,49.41318],[8.65821,49.42225],[8.70053,49.42225],[8.70053,49.42225],[8.65821,49.41318]]]}},"
+            + "{\"type\":\"Feature\",\"properties\":{\"id\":\"Weststadt\"},\"geometry\":{\"type\":\"Polygon\","
+            + "\"coordinates\":[[[8.6801,49.39874],[8.6801,49.40586],[8.69615,49.40586],[8.69615,49.39874],[8.6801,49.39874]]]}}]}";
     InputProcessor iP = new InputProcessor();
     iP.setUtils(new InputProcessingUtils());
     Geometry geom = geomBuilder.createGeometryFromGeoJson(geoJson, iP);
@@ -145,10 +138,10 @@ public class GeometryBuilderTest {
   public void createOverlappingPolygonFromGeoJson() {
 
     String geoJson =
-        "{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"properties\":{},\"geometry\":{\"type\":\"Polygon\","
-            + "\"coordinates\":[[[85.44617,27.999252],[84.418945,27.552113],[86.53931,27.449791],[85.44617,27.999252]]]}},"
-            + "{\"type\":\"Feature\",\"properties\":{},\"geometry\":{\"type\":\"Polygon\","
-            + "\"coordinates\":[[[84.89685,28.130127],[85.02869,27.152033],[86.484375,27.01509],[84.89685,28.130127]]]}}]}";
+        "{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"properties\":{\"id\":\"Neuenheim\"},\"geometry\":{\"type\":\"Polygon\","
+            + "\"coordinates\":[[[8.65821,49.41318],[8.65821,49.42225],[8.70053,49.42225],[8.70053,49.42225],[8.65821,49.41318]]]}},"
+            + "{\"type\":\"Feature\",\"properties\":{\"id\":\"Handschuhsheim\"},\"geometry\":{\"type\":\"Polygon\","
+            + "\"coordinates\":[[[8.67817,49.42147],[8.67817,49.4342],[8.70053,49.4342],[8.70053,49.42147],[8.67817,49.42147]]]}}]}";
     InputProcessor iP = new InputProcessor();
     iP.setUtils(new InputProcessingUtils());
     Geometry geom = geomBuilder.createGeometryFromGeoJson(geoJson, iP);
