53d0f52e288834c3e6b67d4b490d1fa3fe46d825
kowatsch <fabian.kowatsch@uni-heidelberg.de>
2018-09-03 17:35:51 +0200

    adding heidelberg.oshdb file as test-data
    and modifying the tests to use the respective parameters

--- src/test/java/org/heigit/bigspatialdata/ohsome/ohsomeApi/controller/dataAggregation/ControllerTest.java
+++ src/test/java/org/heigit/bigspatialdata/ohsome/ohsomeApi/controller/dataAggregation/ControllerTest.java
@@ -14,7 +14,9 @@ import com.fasterxml.jackson.databind.JsonNode;
 @SpringBootTest(classes = Application.class)
 public class ControllerTest {
   
-  public static final String dbProperty = "--database.db=C:\\Users\\kowatsch\\Desktop\\HeiGIT\\oshdb\\data\\withKeytables\\nepal-z15.oshdb";
+  public static final String dbPropertyPathJenkins = "--database.db=heidelberg.oshdb";
+  // for local testing
+  public static final String dbPropertyPathLocal = "--database.db=C:\\Users\\kowatsch\\Desktop\\HeiGIT\\oshdb\\data\\withKeytables\\heidelberg.oshdb";
 
   /*
    * Simple GET requests
@@ -25,12 +27,12 @@ public class ControllerTest {
 
     // this instance gets reused by all of the following @Test methods
     // and must be in the first length test (otherwise: connection refused error for /length)
-    Application.main(new String[] {dbProperty});
+    Application.main(new String[] {dbPropertyPathJenkins});
     TestRestTemplate restTemplate = new TestRestTemplate();
     ResponseEntity<JsonNode> response = restTemplate.getForEntity("http://localhost:8080"
-        + "/elements/length?bboxes=85.2,27.61,85.45,27.81&types=way&time=2012-01-01&keys=highway&values=residential&showMetadata=true",
+        + "/elements/length?bboxes=8.67452,49.40961,8.70392,49.41823&types=way&time=2012-01-01&keys=highway&values=residential&showMetadata=true",
         JsonNode.class);
-    assertTrue(response.getBody().get("result").get(0).get("value").asDouble() == 336083.1);
+    assertTrue(response.getBody().get("result").get(0).get("value").asDouble() == 15171.81);
   }
 
   @Test
@@ -38,9 +40,9 @@ public class ControllerTest {
 
     TestRestTemplate restTemplate = new TestRestTemplate();
     ResponseEntity<JsonNode> response = restTemplate.getForEntity("http://localhost:8080"
-        + "/elements/count?bboxes=85.2,27.61,85.45,27.81&types=way&time=2015-01-01&keys=building&values=residential&showMetadata=true",
+        + "/elements/count?bboxes=8.67452,49.40961,8.70392,49.41823&types=way&time=2015-01-01&keys=building&values=residential&showMetadata=true",
         JsonNode.class);
-    assertTrue(response.getBody().get("result").get(0).get("value").asInt() == 839);
+    assertTrue(response.getBody().get("result").get(0).get("value").asInt() == 40);
   }
 
   @Test
@@ -48,9 +50,9 @@ public class ControllerTest {
 
     TestRestTemplate restTemplate = new TestRestTemplate();
     ResponseEntity<JsonNode> response = restTemplate.getForEntity("http://localhost:8080"
-        + "/elements/perimeter?bboxes=85.2,27.61,85.45,27.81&types=way&time=2015-01-01&keys=building&values=residential&showMetadata=true",
+        + "/elements/perimeter?bboxes=8.67452,49.40961,8.70392,49.41823&types=way&time=2015-01-01&keys=building&values=residential&showMetadata=true",
         JsonNode.class);
-    assertTrue(response.getBody().get("result").get(0).get("value").asDouble() == 33858.61);
+    assertTrue(response.getBody().get("result").get(0).get("value").asDouble() == 2552.21);
   }
 
   @Test
@@ -58,9 +60,9 @@ public class ControllerTest {
 
     TestRestTemplate restTemplate = new TestRestTemplate();
     ResponseEntity<JsonNode> response = restTemplate.getForEntity("http://localhost:8080"
-        + "/elements/area?bboxes=85.2,27.61,85.45,27.81&types=way&time=2015-01-01&keys=building&values=residential&showMetadata=true",
+        + "/elements/area?bboxes=8.67452,49.40961,8.70392,49.41823&types=way&time=2015-01-01&keys=building&values=residential&showMetadata=true",
         JsonNode.class);
-    assertTrue(response.getBody().get("result").get(0).get("value").asDouble() == 86253.43);
+    assertTrue(response.getBody().get("result").get(0).get("value").asDouble() == 8923.4);
   }
 
   @Test
@@ -68,9 +70,9 @@ public class ControllerTest {
 
     TestRestTemplate restTemplate = new TestRestTemplate();
     ResponseEntity<JsonNode> response = restTemplate.getForEntity("http://localhost:8080"
-        + "/users/count?bboxes=85.2,27.61,85.45,27.81&types=way&time=2014-01-01,2015-01-01&keys=building&values=residential&showMetadata=true",
+        + "/users/count?bboxes=8.67452,49.40961,8.70392,49.41823&types=way&time=2014-01-01,2015-01-01&keys=building&values=residential&showMetadata=true",
         JsonNode.class);
-    assertTrue(response.getBody().get("result").get(0).get("value").asInt() == 29);
+    assertTrue(response.getBody().get("result").get(0).get("value").asInt() == 5);
   }
 
   @Test
@@ -80,7 +82,7 @@ public class ControllerTest {
     ResponseEntity<JsonNode> response =
         restTemplate.getForEntity("http://localhost:8080" + "/metadata", JsonNode.class);
     assertTrue(response.getBody().get("extractRegion").get("temporalExtent").get("toTimestamp")
-        .asText().equals("2018-04-24T19:44:44"));
+        .asText().equals("2018-05-10T18:12:35"));
   }
 
 }
