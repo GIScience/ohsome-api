bd7a442057a667b6d1b44f290c407a95905d3a01
kowatsch <fabian.kowatsch@uni-heidelberg.de>
2018-09-10 14:29:56 +0200

    extending InputProcessingUtilsTest class
    with testing the splitting of the boundary parameters

--- src/test/java/org/heigit/bigspatialdata/ohsome/ohsomeapi/inputprocessing/InputProcessingUtilsTest.java
+++ src/test/java/org/heigit/bigspatialdata/ohsome/ohsomeapi/inputprocessing/InputProcessingUtilsTest.java
@@ -12,7 +12,7 @@ import org.junit.Test;
  * InputProcessingUtils} class.
  */
 public class InputProcessingUtilsTest {
-  
+
   private InputProcessingUtils inProUtils;
 
   @Before
@@ -21,7 +21,67 @@ public class InputProcessingUtilsTest {
     ExtractMetadata.fromTstamp = "2007-11-01";
     ExtractMetadata.toTstamp = "2018-01-01T00:00:00";
   }
-  
+
+  // boundary param split tests
+
+  @Test
+  public void splitBboxesParam() throws Exception {
+    String bboxes = "A:8.67567,49.40695,8.69434,49.40882|B:8.67568,49.40694,8.69433,49.40881";
+    assertTrue(inProUtils.splitBboxes(bboxes).get(5).equals("49.40694"));
+  }
+
+  @Test
+  public void splitBcirclesParam() throws Exception {
+    String bcircles = "A:8.6528,49.3683,1000|B:8.7294,49.4376,1000";
+    assertTrue(inProUtils.splitBcircles(bcircles).get(4).equals("49.4376"));
+  }
+
+  @Test
+  public void splitBpolysParam() throws Exception {
+    String bpolys = "A:8.5992,49.3567,8.7499,49.4371,8.7499,49.4379,8.5992,49.3567|"
+        + "B:9.1638,49.113,9.2672,49.1766,9.2672,49.1775,9.1638,49.113";
+    assertTrue(inProUtils.splitBpolys(bpolys).get(9).equals("49.113"));
+  }
+
+  @Test(expected = BadRequestException.class)
+  public void splitBboxesParamWithAndWithoutColon() throws Exception {
+    String bboxes = "A:8.67567,49.40695,8.69434,49.40882|[idWithoutColon]8.67568,49.40694,"
+        + "8.69433,49.40881";
+    inProUtils.splitBboxes(bboxes);
+  }
+
+  @Test(expected = BadRequestException.class)
+  public void splitBcirclesParamWithAndWithoutColon() throws Exception {
+    String bcircles = "A:8.6528,49.3683,1000|[idWithoutColon]8.7294,49.4376,1000";
+    inProUtils.splitBcircles(bcircles);
+  }
+
+  @Test(expected = BadRequestException.class)
+  public void splitBpolysParamWithAndWithoutColon() throws Exception {
+    String bpolys = "A:8.5992,49.3567,8.7499,49.4371,8.7499,49.4379,8.5992,49.3567|"
+        + "[idWithoutColon]9.1638,49.113,9.2672,49.1766,9.2672,49.1775,9.1638,49.113";
+    inProUtils.splitBpolys(bpolys);
+  }
+
+  @Test(expected = BadRequestException.class)
+  public void splitBboxesParamWithoutPipe() throws Exception {
+    String bboxes = "A:8.67567,49.40695,8.69434,49.40882,B:8.67568,49.40694,8.69433,49.40881";
+    inProUtils.splitBboxes(bboxes);
+  }
+
+  @Test(expected = BadRequestException.class)
+  public void splitBcirclesParamWithoutPipe() throws Exception {
+    String bcircles = "A:8.6528,49.3683,1000,B:8.7294,49.4376,1000";
+    inProUtils.splitBcircles(bcircles);
+  }
+
+  @Test(expected = BadRequestException.class)
+  public void splitBpolysParamWithoutPipe() throws Exception {
+    String bpolys = "A:8.5992,49.3567,8.7499,49.4371,8.7499,49.4379,8.5992,49.3567,B:9.1638,49.113,"
+        + "9.2672,49.1766,9.2672,49.1775,9.1638,49.113";
+    inProUtils.splitBpolys(bpolys);
+  }
+
   // time tests
 
   @Test(expected = BadRequestException.class)
@@ -29,7 +89,7 @@ public class InputProcessingUtilsTest {
     String time = "2015-01-01/2016-01-01/[invalid input]";
     inProUtils.extractIsoTime(time);
   }
-  
+
   @Test
   public void provideIsoConformTimeParameter() throws Exception {
     String time = "2015-01-01/2017-01-01/P1Y";
