6429726da6369df81f38a0141ff80c5451b80f25
kowatsch <fabian.kowatsch@uni-heidelberg.de>
2018-09-14 15:53:56 +0200

    implementing new exception class
    PayloadTooLargeException will get thrown if too much data was requested

---
+++ src/main/java/org/heigit/bigspatialdata/ohsome/ohsomeapi/exception/PayloadTooLargeException.java
@@ -0,0 +1,15 @@
+package org.heigit.bigspatialdata.ohsome.ohsomeapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/** Exception class corresponding to the HTTP status code 413. */
+@ResponseStatus(value = HttpStatus.PAYLOAD_TOO_LARGE)
+public class PayloadTooLargeException extends RuntimeException {
+
+  private static final long serialVersionUID = 3209093385286077205L;
+
+  public PayloadTooLargeException(String message) {
+    super(message);
+  }
+}
