81279dca870fd69003339e7778e4daa1ff9007d6
kowatsch <fabian.kowatsch@uni-heidelberg.de>
2018-09-27 16:11:08 +0200

    renaming test properties
    to better explain their purpose

--- src/test/java/org/heigit/bigspatialdata/ohsome/ohsomeapi/controller/TestProperties.java
+++ src/test/java/org/heigit/bigspatialdata/ohsome/ohsomeapi/controller/TestProperties.java
@@ -4,9 +4,9 @@ package org.heigit.bigspatialdata.ohsome.ohsomeapi.controller;
 public class TestProperties {
 
   public static final String DB_FILE_PATH_PROPERTY = System.getProperty("dbFilePathProperty");
-  public static final String PORT1 = System.getProperty("port1");
-  public static final String PORT2 = System.getProperty("port2");
-  public static final String PORT3 = System.getProperty("port3");
+  public static final String PORT1 = System.getProperty("port_get");
+  public static final String PORT2 = System.getProperty("port_post");
+  public static final String PORT3 = System.getProperty("port_data");
   public static final String JUNIT = System.getProperty("junit");
   public static final String INTEGRATION = System.getProperty("integration");
   public static final String SERVER = "http://localhost:";
