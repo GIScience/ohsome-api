4e9b152b632fbdca68b14df461b5e4321b133a17
kowatsch <fabian.kowatsch@uni-heidelberg.de>
2018-11-07 11:22:26 +0100

    introducing ExceptionMessages class
    which holds custom exception messages for different exceptions

---
+++ src/main/java/org/heigit/bigspatialdata/ohsome/ohsomeapi/exception/ExceptionMessages.java
@@ -0,0 +1,8 @@
+package org.heigit.bigspatialdata.ohsome.ohsomeapi.exception;
+
+/** Holds custom error messages used in several classes for different exceptions. */
+public class ExceptionMessages {
+
+  public static String payloadTooLargeMessage =
+      "The given query is too large. Please use a smaller region and/or coarser time period.";
+}
