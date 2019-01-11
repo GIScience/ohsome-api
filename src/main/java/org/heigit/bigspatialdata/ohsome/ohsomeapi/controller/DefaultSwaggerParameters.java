0e38bc57a0907bb74fb4fecf5b548655f660ee6a
kowatsch <fabian.kowatsch@uni-heidelberg.de>
2019-01-11 16:23:47 +0100

    changing default bbox
    to bbox around Heidelberg

--- src/main/java/org/heigit/bigspatialdata/ohsome/ohsomeapi/controller/DefaultSwaggerParameters.java
+++ src/main/java/org/heigit/bigspatialdata/ohsome/ohsomeapi/controller/DefaultSwaggerParameters.java
@@ -3,7 +3,7 @@ package org.heigit.bigspatialdata.ohsome.ohsomeapi.controller;
 /** Holds the default values for the parameters to run test-requests in Swagger. */
 public class DefaultSwaggerParameters {
 
-  public static final String BBOX = "85.31015,27.71919,85.31828,27.72459";
+  public static final String BBOX = "8.625,49.3711,8.7334,49.4397";
   public static final String TYPE = "way";
   public static final String HIGHWAY_KEY = "highway";
   public static final String BUILDING_KEY = "building";
