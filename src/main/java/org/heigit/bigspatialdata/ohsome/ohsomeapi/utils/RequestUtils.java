47f560d399f0dad3bf052981fd9e8cc0ca75776c
kowatsch <fabian.kowatsch@uni-heidelberg.de>
2019-02-26 16:05:15 +0100

    extending cache http header logic
    no cache is defined when no toTimestamp, the /metadata resource, or data-extraction is given/used

--- src/main/java/org/heigit/bigspatialdata/ohsome/ohsomeapi/utils/RequestUtils.java
+++ src/main/java/org/heigit/bigspatialdata/ohsome/ohsomeapi/utils/RequestUtils.java
@@ -27,4 +27,37 @@ public class RequestUtils {
       return request.getRequestURL() + "?" + queryString;
     }
   }
+
+  /**
+   * Checks, if caching will be allowed for the given <code>HttpServletRequest</code> object, or
+   * not.
+   * 
+   * @param request <code>HttpServletRequest</code> object used to check
+   * @return whether caching is allowed, or not
+   */
+  public static boolean cacheNotAllowed(HttpServletRequest request) {
+    return isMetadata(request) || hasDefaultToTimestamp(request) || isDataExtraction(request);
+  }
+
+  private static boolean hasDefaultToTimestamp(HttpServletRequest request) {
+    String[] time = request.getParameterValues("time");
+    if (time == null || time[0].replaceAll("\\s", "").length() == 0) {
+      return true;
+    }
+    int length = time.length;
+    if (length != 1) {
+      return false;
+    }
+    return (time[0].contains("//") || time[0].endsWith("/"));
+  }
+
+  private static boolean isDataExtraction(HttpServletRequest request) {
+    String url = request.getRequestURL().toString();
+    return (url.contains("elementsFullHistory") || url.contains("elements/geometry")
+        || url.contains("elements/centroid") || url.contains("elements/bbox"));
+  }
+
+  private static boolean isMetadata(HttpServletRequest request) {
+    return request.getRequestURL().toString().contains("/metadata");
+  }
 }
