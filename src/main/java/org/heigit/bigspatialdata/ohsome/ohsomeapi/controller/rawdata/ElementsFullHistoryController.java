3086512c37b4900f22905445bd99fdb539a9a0c0
kowatsch <fabian.kowatsch@uni-heidelberg.de>
2019-03-20 16:57:22 +0100

    updating time parameter description in fullHistory
    to state clearly that only two timestamps defining an interval are allowed as input
    setting the default value to a smaller range of 1 year

--- src/main/java/org/heigit/bigspatialdata/ohsome/ohsomeapi/controller/rawdata/ElementsFullHistoryController.java
+++ src/main/java/org/heigit/bigspatialdata/ohsome/ohsomeapi/controller/rawdata/ElementsFullHistoryController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 
 
@@ -30,6 +31,10 @@ public class ElementsFullHistoryController {
   @ApiOperation(
       value = "Full-history OSM data having the raw geometry of each OSM object as geometry",
       nickname = "fullHistory", response = DataResponse.class)
+  @ApiImplicitParam(name = "time",
+      value = "Two ISO-8601 conform timestrings defining an interval; no default value",
+      defaultValue = "2016-01-01,2017-01-01", paramType = "query", dataType = "string",
+      required = true)
   @RequestMapping(value = "/geometry", method = {RequestMethod.GET, RequestMethod.POST},
       produces = "application/json")
   public void elementsFullHistory(HttpServletRequest servletRequest,
@@ -48,6 +53,10 @@ public class ElementsFullHistoryController {
   @ApiOperation(
       value = "Full-history OSM data, having the bounding box of each OSM object as geometry",
       nickname = "bboxFullHistory", response = DataResponse.class)
+  @ApiImplicitParam(name = "time",
+      value = "Two ISO-8601 conform timestrings defining an interval; no default value",
+      defaultValue = "2016-01-01,2017-01-01", paramType = "query", dataType = "string",
+      required = true)
   @RequestMapping(value = "/bbox", method = {RequestMethod.GET, RequestMethod.POST},
       produces = "application/json")
   public void elementsBboxFullHistory(HttpServletRequest servletRequest,
@@ -65,6 +74,10 @@ public class ElementsFullHistoryController {
    */
   @ApiOperation(value = "Full-history OSM data, having the centroid of each OSM object as geometry",
       nickname = "centroidFullHistory", response = DataResponse.class)
+  @ApiImplicitParam(name = "time",
+      value = "Two ISO-8601 conform timestrings defining an interval; no default value",
+      defaultValue = "2016-01-01,2017-01-01", paramType = "query", dataType = "string",
+      required = true)
   @RequestMapping(value = "/centroid", method = {RequestMethod.GET, RequestMethod.POST},
       produces = "application/json")
   public void elementsCentroidFullHistory(HttpServletRequest servletRequest,
