0987eb45b2951101f473e661930d1d7b7d4dc537
kowatsch <fabian.kowatsch@uni-heidelberg.de>
2019-06-18 09:23:57 +0200

    update comment
    moving it from variable to the actual class

--- src/main/java/org/heigit/bigspatialdata/ohsome/ohsomeapi/oshdb/RemoteTagTranslator.java
+++ src/main/java/org/heigit/bigspatialdata/ohsome/ohsomeapi/oshdb/RemoteTagTranslator.java
@@ -4,6 +4,10 @@ import org.heigit.bigspatialdata.oshdb.api.generic.function.SerializableSupplier
 import org.heigit.bigspatialdata.oshdb.util.celliterator.LazyEvaluatedObject;
 import org.heigit.bigspatialdata.oshdb.util.tagtranslator.TagTranslator;
 
+/**
+ * A tag-translator that can be used in map/flatMap/aggregateBy functions that may be executed on
+ * remote machines.
+ */
 public class RemoteTagTranslator extends LazyEvaluatedObject<TagTranslator> {
 
   public RemoteTagTranslator(SerializableSupplier<TagTranslator> evaluator) {
