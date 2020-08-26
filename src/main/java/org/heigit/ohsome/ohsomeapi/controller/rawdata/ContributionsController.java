8e6724432b1e446702d5cc6ad39641d621547221
Fabian Kowatsch <fabian.kowatsch@heigit.org>
2020-08-26 12:46:22 +0200

    adding further endpoints under /contributions
    as well as a request executor class

--- src/main/java/org/heigit/ohsome/ohsomeapi/controller/rawdata/ContributionsController.java
+++ src/main/java/org/heigit/ohsome/ohsomeapi/controller/rawdata/ContributionsController.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.heigit.ohsome.ohsomeapi.executor.ContributionsRequestExecutor;
 import org.heigit.ohsome.ohsomeapi.output.rawdataresponse.DataResponse;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -37,6 +38,52 @@ public class ContributionsController {
       produces = "application/json")
   public void contributions(HttpServletRequest servletRequest,
       HttpServletResponse servletResponse) throws Exception {
+    ContributionsRequestExecutor.extract(ElementsGeometry.RAW, servletRequest,
+        servletResponse);
+  }
+  
+  /**
+   * Gives the contributions as GeoJSON features, which have the bounding box of the respective
+   * objects in the geometry field.
+   * 
+   * @param servletRequest <code>HttpServletRequest</code> of the incoming request
+   * @param servletResponse <code>HttpServletResponse</code> of the outgoing response
+   */
+  @ApiOperation(
+      value = "OSM contributions having the bounding box of each OSM object as geometry",
+      nickname = "contributionBbox", response = DataResponse.class)
+  @ApiImplicitParam(name = "time",
+      value = "Two ISO-8601 conform timestrings defining an interval; no default value",
+      defaultValue = "2016-01-01,2017-01-01", paramType = "query", dataType = "string",
+      required = true)
+  @RequestMapping(value = "/bbox", method = {RequestMethod.GET, RequestMethod.POST},
+      produces = "application/json")
+  public void contributionsBbox(HttpServletRequest servletRequest,
+      HttpServletResponse servletResponse) throws Exception {
+    ContributionsRequestExecutor.extract(ElementsGeometry.BBOX, servletRequest,
+        servletResponse);
+  }
+  
+  /**
+   * Gives the contributions as GeoJSON features, which have the centroid of the respective objects
+   * in the geometry field.
+   * 
+   * @param servletRequest <code>HttpServletRequest</code> of the incoming request
+   * @param servletResponse <code>HttpServletResponse</code> of the outgoing response
+   */
+  @ApiOperation(
+      value = "OSM contributions having the centroid of each OSM object as geometry",
+      nickname = "contributionCentroid", response = DataResponse.class)
+  @ApiImplicitParam(name = "time",
+      value = "Two ISO-8601 conform timestrings defining an interval; no default value",
+      defaultValue = "2016-01-01,2017-01-01", paramType = "query", dataType = "string",
+      required = true)
+  @RequestMapping(value = "/centroid", method = {RequestMethod.GET, RequestMethod.POST},
+      produces = "application/json")
+  public void contributionsCentroid(HttpServletRequest servletRequest,
+      HttpServletResponse servletResponse) throws Exception {
+    ContributionsRequestExecutor.extract(ElementsGeometry.CENTROID, servletRequest,
+        servletResponse);
   }
 
 }
---
+++ src/main/java/org/heigit/ohsome/ohsomeapi/executor/ContributionsRequestExecutor.java
@@ -0,0 +1,14 @@
+package org.heigit.ohsome.ohsomeapi.executor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.heigit.ohsome.ohsomeapi.controller.rawdata.ElementsGeometry;
+
+public class ContributionsRequestExecutor {
+
+  public static void extract(ElementsGeometry elemGeom, HttpServletRequest servletRequest,
+      HttpServletResponse servletResponse) throws Exception {
+    
+  }
+  
+}
