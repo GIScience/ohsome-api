22e60d2796907ae5d0685d5bde2876c8860ca761
Fabian Kowatsch <fabian.kowatsch@heigit.org>
2020-09-04 16:55:08 +0200

    using new feature from Java 11
    replacing long variable declarations with keyword "var"
    moving some variable declarations closer to their point of initialization

--- src/main/java/org/heigit/ohsome/ohsomeapi/executor/ElementsRequestExecutor.java
+++ src/main/java/org/heigit/ohsome/ohsomeapi/executor/ElementsRequestExecutor.java
@@ -9,7 +9,6 @@ import java.util.EnumSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
@@ -404,10 +403,9 @@ public class ElementsRequestExecutor {
       mapAgg = inputProcessor.filterOnGeometryType(mapAgg, filter.get());
     }
     ExecutionUtils exeUtils = new ExecutionUtils(processingData);
-    MapAggregator<OSHDBCombinedIndex<OSHDBCombinedIndex<Integer, Pair<Integer, Integer>>, OSHDBTimestamp>, Geometry> preResult =
-        mapAgg.map(f -> exeUtils.mapSnapshotToTags(keysInt, valuesInt, f))
-            .aggregateBy(Pair::getKey, zeroFill).map(Pair::getValue)
-            .aggregateByTimestamp(OSMEntitySnapshot::getTimestamp).map(x -> x.getGeometry());
+    var preResult = mapAgg.map(f -> exeUtils.mapSnapshotToTags(keysInt, valuesInt, f))
+        .aggregateBy(Pair::getKey, zeroFill).map(Pair::getValue)
+        .aggregateByTimestamp(OSMEntitySnapshot::getTimestamp).map(x -> x.getGeometry());
     var result = exeUtils.computeNestedResult(requestResource, preResult);
     var groupByResult = OSHDBCombinedIndex.nest(result);
     GroupByResult[] resultSet = new GroupByResult[groupByResult.entrySet().size()];
@@ -415,8 +413,7 @@ public class ElementsRequestExecutor {
     Object[] boundaryIds = utils.getBoundaryIds();
     int count = 0;
     ArrayList<Geometry> boundaries = new ArrayList<>(processingData.getBoundaryList());
-    for (Entry<OSHDBCombinedIndex<Integer, Pair<Integer, Integer>>, ? extends SortedMap<OSHDBTimestamp, ? extends Number>> entry : groupByResult
-        .entrySet()) {
+    for (var entry : groupByResult.entrySet()) {
       int boundaryIdentifier = entry.getKey().getFirstIndex();
       ElementsResult[] results = exeUtils.fillElementsResult(entry.getValue(),
           requestParameters.isDensity(), df, boundaries.get(boundaryIdentifier));
@@ -501,19 +498,15 @@ public class ElementsRequestExecutor {
         zeroFill.add(new ImmutablePair<Integer, Integer>(keysInt, valuesInt[j]));
       }
     }
-    MapAggregator<OSHDBCombinedIndex<OSHDBTimestamp, Pair<Integer, Integer>>, OSMEntitySnapshot> preResult =
-        mapRed.map(f -> exeUtils.mapSnapshotToTags(keysInt, valuesInt, f)).aggregateByTimestamp()
-            .aggregateBy(Pair::getKey, zeroFill).map(Pair::getValue);
-    SortedMap<OSHDBCombinedIndex<OSHDBTimestamp, Pair<Integer, Integer>>, ? extends Number> result;
-    SortedMap<Pair<Integer, Integer>, ? extends SortedMap<OSHDBTimestamp, ? extends Number>> groupByResult;
-    result = exeUtils.computeResult(requestResource, preResult);
-    groupByResult = ExecutionUtils.nest(result);
+    var preResult = mapRed.map(f -> exeUtils.mapSnapshotToTags(keysInt, valuesInt, f))
+        .aggregateByTimestamp().aggregateBy(Pair::getKey, zeroFill).map(Pair::getValue);
+    var result = exeUtils.computeResult(requestResource, preResult);
+    var groupByResult = ExecutionUtils.nest(result);
     GroupByResult[] resultSet = new GroupByResult[groupByResult.size()];
     String groupByName = "";
     Geometry geom = inputProcessor.getGeometry();
     int count = 0;
-    for (Entry<Pair<Integer, Integer>, ? extends SortedMap<OSHDBTimestamp, ? extends Number>> entry : groupByResult
-        .entrySet()) {
+    for (var entry : groupByResult.entrySet()) {
       ElementsResult[] results =
           exeUtils.fillElementsResult(entry.getValue(), requestParameters.isDensity(), df, geom);
       // check for non-remainder objects (which do have the defined key and value)
@@ -578,15 +571,12 @@ public class ElementsRequestExecutor {
         .aggregateBy((SerializableFunction<OSMEntitySnapshot, OSMType>) f -> {
           return f.getEntity().getType();
         }, processingData.getOsmTypes());
-    SortedMap<OSHDBCombinedIndex<OSHDBTimestamp, OSMType>, ? extends Number> result;
-    result = exeUtils.computeResult(requestResource, preResult);
-    SortedMap<OSMType, ? extends SortedMap<OSHDBTimestamp, ? extends Number>> groupByResult;
-    groupByResult = ExecutionUtils.nest(result);
+    var result = exeUtils.computeResult(requestResource, preResult);
+    var groupByResult = ExecutionUtils.nest(result);
     GroupByResult[] resultSet = new GroupByResult[groupByResult.size()];
     Geometry geom = inputProcessor.getGeometry();
     int count = 0;
-    for (Entry<OSMType, ? extends SortedMap<OSHDBTimestamp, ? extends Number>> entry : groupByResult
-        .entrySet()) {
+    for (var entry : groupByResult.entrySet()) {
       ElementsResult[] results =
           exeUtils.fillElementsResult(entry.getValue(), requestParameters.isDensity(), df, geom);
       resultSet[count] = new GroupByResult(entry.getKey().toString(), results);
@@ -666,15 +656,12 @@ public class ElementsRequestExecutor {
           return res;
         }).aggregateByTimestamp().aggregateBy(Pair::getKey, Arrays.asList(keysInt))
             .map(Pair::getValue);
-    SortedMap<OSHDBCombinedIndex<OSHDBTimestamp, Integer>, ? extends Number> result;
-    SortedMap<Integer, ? extends SortedMap<OSHDBTimestamp, ? extends Number>> groupByResult;
-    result = exeUtils.computeResult(requestResource, preResult);
-    groupByResult = ExecutionUtils.nest(result);
+    var result = exeUtils.computeResult(requestResource, preResult);
+    var groupByResult = ExecutionUtils.nest(result);
     GroupByResult[] resultSet = new GroupByResult[groupByResult.size()];
     String groupByName = "";
     int count = 0;
-    for (Entry<Integer, ? extends SortedMap<OSHDBTimestamp, ? extends Number>> entry : groupByResult
-        .entrySet()) {
+    for (var entry : groupByResult.entrySet()) {
       ElementsResult[] results =
           exeUtils.fillElementsResult(entry.getValue(), requestParameters.isDensity(), df, null);
       // check for non-remainder objects (which do have the defined key)
@@ -799,8 +786,7 @@ public class ElementsRequestExecutor {
     mapRed = mapRed.osmType(osmTypes);
     mapRed = tempExecutionUtils.snapshotFilter(mapRed, osmTypes1, osmTypes2, simpleFeatureTypes1,
         simpleFeatureTypes2, keysInt1, keysInt2, valuesInt1, valuesInt2);
-    MapAggregator<OSHDBCombinedIndex<OSHDBTimestamp, MatchType>, OSMEntitySnapshot> preResult;
-    preResult = mapRed.aggregateByTimestamp().aggregateBy(snapshot -> {
+    var preResult = mapRed.aggregateByTimestamp().aggregateBy(snapshot -> {
       boolean matches1 = tempExecutionUtils.snapshotMatches(snapshot, osmTypes1,
           simpleFeatureTypes1, keysInt1, valuesInt1);
       boolean matches2 = tempExecutionUtils.snapshotMatches(snapshot, osmTypes2,
@@ -817,9 +803,8 @@ public class ElementsRequestExecutor {
         return MatchType.MATCHESNONE;
       }
     }, EnumSet.allOf(MatchType.class));
-    SortedMap<OSHDBCombinedIndex<OSHDBTimestamp, MatchType>, ? extends Number> result = null;
     ExecutionUtils exeUtils = new ExecutionUtils(processingData);
-    result = exeUtils.computeResult(requestResource, preResult);
+    var result = exeUtils.computeResult(requestResource, preResult);
     int resultSize = result.size();
     Double[] value1 = new Double[resultSize / 4];
     Double[] value2 = new Double[resultSize / 4];
@@ -828,8 +813,7 @@ public class ElementsRequestExecutor {
     int value2Count = 0;
     int matchesBothCount = 0;
     // time and value extraction
-    for (Entry<OSHDBCombinedIndex<OSHDBTimestamp, MatchType>, ? extends Number> entry : result
-        .entrySet()) {
+    for (var entry : result.entrySet()) {
       if (entry.getKey().getSecondIndex() == MatchType.MATCHES2) {
         timeArray[value2Count] =
             TimestampFormatter.getInstance().isoDateTime(entry.getKey().getFirstIndex());
@@ -906,8 +890,7 @@ public class ElementsRequestExecutor {
     MapReducer<OSMEntitySnapshot> mapRed = inputProcessorCombined.processParameters();
 
     mapRed = exeUtils.newSnapshotFilter(mapRed, filterExpr1, filterExpr2);
-    MapAggregator<OSHDBCombinedIndex<OSHDBTimestamp, MatchType>, OSMEntitySnapshot> preResult;
-    preResult = mapRed.aggregateByTimestamp().aggregateBy(snapshot -> {
+    var preResult = mapRed.aggregateByTimestamp().aggregateBy(snapshot -> {
       OSMEntity entity = snapshot.getEntity();
       boolean matches1 = filterExpr1.applyOSMGeometry(entity, snapshot.getGeometry());
       boolean matches2 = filterExpr2.applyOSMGeometry(entity, snapshot.getGeometry());
@@ -923,8 +906,7 @@ public class ElementsRequestExecutor {
         return MatchType.MATCHESNONE;
       }
     }, EnumSet.allOf(MatchType.class));
-    SortedMap<OSHDBCombinedIndex<OSHDBTimestamp, MatchType>, ? extends Number> result = null;
-    result = exeUtils.computeResult(requestResource, preResult);
+    var result = exeUtils.computeResult(requestResource, preResult);
     int resultSize = result.size();
     int matchTypeSize = 4;
     Double[] value1 = new Double[resultSize / matchTypeSize];
@@ -934,8 +916,7 @@ public class ElementsRequestExecutor {
     int value2Count = 0;
     int matchesBothCount = 0;
     // time and value extraction
-    for (Entry<OSHDBCombinedIndex<OSHDBTimestamp, MatchType>, ? extends Number> entry : result
-        .entrySet()) {
+    for (var entry : result.entrySet()) {
       if (entry.getKey().getSecondIndex() == MatchType.MATCHES2) {
         timeArray[value2Count] =
             TimestampFormatter.getInstance().isoDateTime(entry.getKey().getFirstIndex());
@@ -982,8 +963,6 @@ public class ElementsRequestExecutor {
     final long startTime = System.currentTimeMillis();
     final boolean isSnapshot = true;
     final boolean isDensity = false;
-    SortedMap<OSHDBCombinedIndex<OSHDBCombinedIndex<OSHDBTimestamp, Integer>, MatchType>, ? extends Number> result =
-        null;
     InputProcessor inputProcessor = new InputProcessor(servletRequest, isSnapshot, isDensity);
     inputProcessor.getProcessingData().setIsGroupByBoundary(true);
     inputProcessor.getProcessingData().setIsRatio(true);
@@ -1058,18 +1037,15 @@ public class ElementsRequestExecutor {
     }
     mapRed = mapRed.osmType(osmTypes);
     ArrayList<Geometry> arrGeoms = new ArrayList<>(processingData.getBoundaryList());
-    MapAggregator<OSHDBCombinedIndex<OSHDBCombinedIndex<OSHDBTimestamp, Integer>, MatchType>, Geometry> preResult =
-        null;
     // intentionally as check for P on Polygonal is already performed
     @SuppressWarnings({"unchecked"})
     Map<Integer, P> geoms = arrGeoms.stream()
         .collect(Collectors.toMap(geom -> arrGeoms.indexOf(geom), geom -> (P) geom));
     ExecutionUtils exeUtils = new ExecutionUtils(processingData);
-    MapAggregator<OSHDBCombinedIndex<OSHDBTimestamp, Integer>, OSMEntitySnapshot> mapRed2 =
-        mapRed.aggregateByTimestamp().aggregateByGeometry(geoms);
+    var mapRed2 = mapRed.aggregateByTimestamp().aggregateByGeometry(geoms);
     mapRed2 = exeUtils.snapshotFilter(mapRed2, osmTypes1, osmTypes2, simpleFeatureTypes1,
         simpleFeatureTypes2, keysInt1, keysInt2, valuesInt1, valuesInt2);
-    preResult =
+    var preResult =
         mapRed2.aggregateBy((SerializableFunction<OSMEntitySnapshot, MatchType>) snapshot -> {
           boolean matches1 = exeUtils.snapshotMatches(snapshot, osmTypes1, simpleFeatureTypes1,
               keysInt1, valuesInt1);
@@ -1086,6 +1062,8 @@ public class ElementsRequestExecutor {
           }
           return MatchType.MATCHESNONE;
         }, EnumSet.allOf(MatchType.class)).map(x -> x.getGeometry());
+    SortedMap<OSHDBCombinedIndex<OSHDBCombinedIndex<OSHDBTimestamp, Integer>, MatchType>, ? extends Number> result =
+        null;
     switch (requestResource) {
       case COUNT:
         result = preResult.count();
@@ -1111,18 +1089,15 @@ public class ElementsRequestExecutor {
       default:
         break;
     }
-    SortedMap<MatchType, ? extends SortedMap<OSHDBCombinedIndex<OSHDBTimestamp, Integer>, ? extends Number>> groupByResult;
     InputProcessingUtils utils = inputProcessor.getUtils();
-    groupByResult = ExecutionUtils.nest(result);
+    var groupByResult = ExecutionUtils.nest(result);
     Object[] boundaryIds = utils.getBoundaryIds();
     Double[] resultValues1 = null;
     Double[] resultValues2 = null;
     String[] timeArray = null;
     boolean timeArrayFilled = false;
-    for (Entry<MatchType, ? extends SortedMap<OSHDBCombinedIndex<OSHDBTimestamp, Integer>, ? extends Number>> entry : groupByResult
-        .entrySet()) {
-      Set<? extends Entry<OSHDBCombinedIndex<OSHDBTimestamp, Integer>, ? extends Number>> resultSet =
-          entry.getValue().entrySet();
+    for (var entry : groupByResult.entrySet()) {
+      var resultSet = entry.getValue().entrySet();
       if (!timeArrayFilled) {
         timeArray = new String[resultSet.size()];
       }
@@ -1133,7 +1108,7 @@ public class ElementsRequestExecutor {
       } else if (entry.getKey() == MatchType.MATCHESBOTH) {
         int matchesBothCount = 0;
         int timeArrayCount = 0;
-        for (Entry<OSHDBCombinedIndex<OSHDBTimestamp, Integer>, ? extends Number> innerEntry : resultSet) {
+        for (var innerEntry : resultSet) {
           resultValues1[matchesBothCount] = resultValues1[matchesBothCount]
               + Double.parseDouble(df.format(innerEntry.getValue().doubleValue()));
           resultValues2[matchesBothCount] = resultValues2[matchesBothCount]
@@ -1219,20 +1194,16 @@ public class ElementsRequestExecutor {
     inputProcessorCombined.getProcessingData().setIsGroupByBoundary(true);
     MapReducer<OSMEntitySnapshot> mapRed = inputProcessorCombined.processParameters();
 
-    MapAggregator<OSHDBCombinedIndex<OSHDBCombinedIndex<OSHDBTimestamp, Integer>, MatchType>, Geometry> preResult =
-        null;
-    SortedMap<OSHDBCombinedIndex<OSHDBCombinedIndex<OSHDBTimestamp, Integer>, MatchType>, ? extends Number> result =
-        null;
+
     ArrayList<Geometry> arrGeoms = new ArrayList<>(processingData.getBoundaryList());
     // intentionally as check for P on Polygonal is already performed
     @SuppressWarnings({"unchecked"})
     Map<Integer, P> geoms = arrGeoms.stream()
         .collect(Collectors.toMap(geom -> arrGeoms.indexOf(geom), geom -> (P) geom));
-    MapAggregator<OSHDBCombinedIndex<OSHDBTimestamp, Integer>, OSMEntitySnapshot> mapRed2 =
-        mapRed.aggregateByTimestamp().aggregateByGeometry(geoms);
+    var mapRed2 = mapRed.aggregateByTimestamp().aggregateByGeometry(geoms);
 
     mapRed2 = exeUtils.newSnapshotFilter(mapRed2, filterExpr1, filterExpr2);
-    preResult =
+    var preResult =
         mapRed2.aggregateBy((SerializableFunction<OSMEntitySnapshot, MatchType>) snapshot -> {
           OSMEntity entity = snapshot.getEntity();
           boolean matches1 = filterExpr1.applyOSMGeometry(entity, snapshot.getGeometry());
@@ -1248,6 +1219,8 @@ public class ElementsRequestExecutor {
           }
           return MatchType.MATCHESNONE;
         }, EnumSet.allOf(MatchType.class)).map(x -> x.getGeometry());
+    SortedMap<OSHDBCombinedIndex<OSHDBCombinedIndex<OSHDBTimestamp, Integer>, MatchType>, ? extends Number> result =
+        null;
     switch (requestResource) {
       case COUNT:
         result = preResult.count();
@@ -1273,18 +1246,15 @@ public class ElementsRequestExecutor {
       default:
         break;
     }
-    SortedMap<MatchType, ? extends SortedMap<OSHDBCombinedIndex<OSHDBTimestamp, Integer>, ? extends Number>> groupByResult;
     InputProcessingUtils utils = inputProcessor.getUtils();
-    groupByResult = ExecutionUtils.nest(result);
+    var groupByResult = ExecutionUtils.nest(result);
     Object[] boundaryIds = utils.getBoundaryIds();
     Double[] resultValues1 = null;
     Double[] resultValues2 = null;
     String[] timeArray = null;
     boolean timeArrayFilled = false;
-    for (Entry<MatchType, ? extends SortedMap<OSHDBCombinedIndex<OSHDBTimestamp, Integer>, ? extends Number>> entry : groupByResult
-        .entrySet()) {
-      Set<? extends Entry<OSHDBCombinedIndex<OSHDBTimestamp, Integer>, ? extends Number>> resultSet =
-          entry.getValue().entrySet();
+    for (var entry : groupByResult.entrySet()) {
+      var resultSet = entry.getValue().entrySet();
       if (!timeArrayFilled) {
         timeArray = new String[resultSet.size()];
       }
@@ -1295,7 +1265,7 @@ public class ElementsRequestExecutor {
       } else if (entry.getKey() == MatchType.MATCHESBOTH) {
         int matchesBothCount = 0;
         int timeArrayCount = 0;
-        for (Entry<OSHDBCombinedIndex<OSHDBTimestamp, Integer>, ? extends Number> innerEntry : resultSet) {
+        for (var innerEntry : resultSet) {
           resultValues1[matchesBothCount] = resultValues1[matchesBothCount]
               + Double.parseDouble(df.format(innerEntry.getValue().doubleValue()));
           resultValues2[matchesBothCount] = resultValues2[matchesBothCount]
