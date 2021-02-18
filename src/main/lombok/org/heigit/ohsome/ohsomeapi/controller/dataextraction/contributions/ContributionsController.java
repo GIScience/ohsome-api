2dc97a165fb8173b3d04b07976acc862bb226544
Fabian Kowatsch <fabian.kowatsch@heigit.org>
2021-02-18 15:00:23 +0100

    adapt structure from master
    apply new package structure and merge it with new endpoint
    adapt swagger structure

--- src/main/lombok/org/heigit/ohsome/ohsomeapi/controller/dataextraction/contributions/ContributionsController.java
+++ src/main/lombok/org/heigit/ohsome/ohsomeapi/controller/dataextraction/contributions/ContributionsController.java
@@ -6,12 +6,9 @@ import io.swagger.annotations.ApiOperation;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.heigit.ohsome.ohsomeapi.controller.dataextraction.elements.ElementsGeometry;
-import org.heigit.ohsome.ohsomeapi.executor.ContributionsExecutor;
 import org.heigit.ohsome.ohsomeapi.executor.DataRequestExecutor;
 import org.heigit.ohsome.ohsomeapi.executor.RequestResource;
-import org.heigit.ohsome.ohsomeapi.output.DefaultAggregationResponse;
 import org.heigit.ohsome.ohsomeapi.output.ExtractionResponse;
-import org.heigit.ohsome.ohsomeapi.output.Response;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -95,46 +92,6 @@ public class ContributionsController {
     DataRequestExecutor executor = new DataRequestExecutor(RequestResource.CONTRIBUTIONS,
         ElementsGeometry.CENTROID, servletRequest, servletResponse);
     executor.extract();
-  }
-  
-  /**
-   * Gives the count of OSM contributions as JSON.
-   * 
-   * @param servletRequest <code>HttpServletRequest</code> of the incoming request
-   * @param servletResponse <code>HttpServletResponse</code> of the outgoing response
-   * @return {@link org.heigit.ohsome.ohsomeapi.output.dataaggregationresponse.Response Response}
-   * @throws Exception thrown by
-   *         {@link org.heigit.ohsome.ohsomeapi.executor.ContributionsExecutor#count(boolean) count}
-   */
-  @ApiOperation(value = "count of OSM contributions", nickname = "contributionsCount",
-      response = DefaultAggregationResponse.class)
-  @RequestMapping(value = "/count", method = {RequestMethod.GET, RequestMethod.POST},
-      produces = {"application/json", "text/csv"})
-  public Response contributionsCount(HttpServletRequest servletRequest,
-      HttpServletResponse servletResponse) throws Exception {
-    ContributionsExecutor executor =
-        new ContributionsExecutor(servletRequest, servletResponse, false);
-    return executor.count(false);
-  }
-  
-  /**
-   * Gives the density of OSM contributions as JSON.
-   * 
-   * @param servletRequest <code>HttpServletRequest</code> of the incoming request
-   * @param servletResponse <code>HttpServletResponse</code> of the outgoing response
-   * @return {@link org.heigit.ohsome.ohsomeapi.output.dataaggregationresponse.Response Response}
-   * @throws Exception thrown by
-   *         {@link org.heigit.ohsome.ohsomeapi.executor.ContributionsExecutor#count(boolean) count}
-   */
-  @ApiOperation(value = "density of OSM contributions", nickname = "contributionsCountDensity",
-      response = DefaultAggregationResponse.class)
-  @RequestMapping(value = "/count/density", method = {RequestMethod.GET, RequestMethod.POST},
-      produces = {"application/json", "text/csv"})
-  public Response contributionsCountDensity(HttpServletRequest servletRequest,
-      HttpServletResponse servletResponse) throws Exception {
-    ContributionsExecutor executor =
-        new ContributionsExecutor(servletRequest, servletResponse, true);
-    return executor.count(false);
   }
   
   /**
