b90edf5b49f4b5a98c3f658feb99f9cb8a16d1a1
Martin Raifer <martin@raifer.tech>
2021-04-16 17:35:06 +0200

    reorder constructor parameters
    * first query settings ("what is requested")
    * then endpoint ("how should it be processed")
    * then output settings ("how should it be returned")
    * then utility objects
    * lastly, deprecated stuff

--- src/main/lombok/org/heigit/ohsome/ohsomeapi/executor/DataExtractionTransformer.java
+++ src/main/lombok/org/heigit/ohsome/ohsomeapi/executor/DataExtractionTransformer.java
@@ -25,75 +25,72 @@ import org.wololo.geojson.Feature;
  */
 public class DataExtractionTransformer implements Serializable {
 
-  private final boolean isContributionsLatestEndpoint;
-  private final boolean isContributionsEndpoint;
-  private final ExecutionUtils exeUtils;
-  private final boolean clipGeometries;
   private final String startTimestamp;
-  private final InputProcessingUtils utils;
-  @Deprecated
-  private final Set<SimpleFeatureType> simpleFeatureTypes;
+  private final String endTimestamp;
   private final FilterExpression filter;
-  @Deprecated
-  private final Set<Integer> keysInt;
+  private final boolean isContributionsEndpoint;
+  private final boolean isContributionsLatestEndpoint;
+  private final boolean clipGeometries;
   private final boolean includeTags;
   private final boolean includeOSMMetadata;
   private final boolean includeContributionTypes;
-  private final ElementsGeometry elementsGeometry;
-  private final String endTimestamp;
+  private final ElementsGeometry outputGeometry;
+  private final InputProcessingUtils inputUtils;
+  private final ExecutionUtils exeUtils;
+  @Deprecated
+  private final Set<Integer> keysInt;
+  @Deprecated
+  private final Set<SimpleFeatureType> simpleFeatureTypes;
   @Deprecated
   private final boolean isContainingSimpleFeatureTypes;
 
   /**
    * Creates a new data extraction transformer, adhering to the given parameters.
-   *
-   * @param isContributionsLatestEndpoint set true if the requested resource is a
-   *                                      `/contributions/latest` endpoint
-   * @param isContributionsEndpoint set true if the requested resource is a
-   *                                `/contributions` endpoint
-   * @param exeUtils the execution utils object
-   * @param clipGeometries whether or not the output geometries should be clipped to the query
-   *                       area-of-interest or not
    * @param startTimestamp start timestamp of the query
-   * @param utils input processing utility object
-   * @param simpleFeatureTypes if the query uses the (deprecated) types parameter, and it contains
-   *                           simple feature "geometry" types, specify the set of to be returned
-   *                           geometry types here
-   * @param filter the filter of the query
-   * @param keysInt (for the deprecated `keys` filter parameter) set the list of always to be
-   *                returned OSM tags in the GeoJSON's features' properties
-   * @param includeTags set true if the result should include all OSM entity's tags as GeoJSON
-   *                    feature properties
-   * @param includeOSMMetadata set true if the result should include the OSM entity metadata (e.g.
-   *                           cahngeset id, timestamp, version number)
-   * @param includeContributionTypes set true if the result should include the contribution type
-   *                                 for `/elements/contributions` resources
-   * @param elementsGeometry specifies what should be returned as the GeoJSON feature's geometry:
-   *                         either the full geometry, its bbox or its centroid.
    * @param endTimestamp end timestamp of the query
+   * @param filter the filter of the query
+   * @param isContributionsEndpoint set true if the requested resource is a
+*                                `/contributions` endpoint
+   * @param isContributionsLatestEndpoint set true if the requested resource is a
+*                                      `/contributions/latest` endpoint
+   * @param clipGeometries whether or not the output geometries should be clipped to the query
+*                       area-of-interest or not
+   * @param includeTags set true if the result should include all OSM entity's tags as GeoJSON
+*                    feature properties
+   * @param includeOSMMetadata set true if the result should include the OSM entity metadata (e.g.
+*                           cahngeset id, timestamp, version number)
+   * @param includeContributionTypes set true if the result should include the contribution type
+*                                 for `/elements/contributions` resources
+   * @param inputUtils input processing utility object
+   * @param exeUtils the execution utils object
+   * @param keysInt (for the deprecated `keys` filter parameter) set the list of always to be
+*                returned OSM tags in the GeoJSON's features' properties
+   * @param outputGeometry specifies what should be returned as the GeoJSON feature's geometry:
+*                         either the full geometry, its bbox or its centroid.
+   * @param simpleFeatureTypes if the query uses the (deprecated) types parameter, and it contains
+*                           simple feature "geometry" types, specify the set of to be returned
+*                           geometry types here
    * @param isContainingSimpleFeatureTypes set true if the query uses the (deprecated) types
-   *                                       parameter and it contains simple feature "geometry" types
    */
-  public DataExtractionTransformer(boolean isContributionsLatestEndpoint,
-      boolean isContributionsEndpoint, ExecutionUtils exeUtils, boolean clipGeometries,
-      String startTimestamp, InputProcessingUtils utils, Set<SimpleFeatureType> simpleFeatureTypes,
-      FilterExpression filter, Set<Integer> keysInt, boolean includeTags,
-      boolean includeOSMMetadata, boolean includeContributionTypes,
-      ElementsGeometry elementsGeometry, String endTimestamp,
-      boolean isContainingSimpleFeatureTypes) {
+  public DataExtractionTransformer(String startTimestamp, String endTimestamp,
+      FilterExpression filter, boolean isContributionsEndpoint,
+      boolean isContributionsLatestEndpoint, boolean clipGeometries, boolean includeTags,
+      boolean includeOSMMetadata, boolean includeContributionTypes, InputProcessingUtils inputUtils,
+      ExecutionUtils exeUtils, Set<Integer> keysInt, ElementsGeometry outputGeometry,
+      Set<SimpleFeatureType> simpleFeatureTypes, boolean isContainingSimpleFeatureTypes) {
     this.isContributionsLatestEndpoint = isContributionsLatestEndpoint;
     this.isContributionsEndpoint = isContributionsEndpoint;
     this.exeUtils = exeUtils;
     this.clipGeometries = clipGeometries;
     this.startTimestamp = startTimestamp;
-    this.utils = utils;
+    this.inputUtils = inputUtils;
     this.simpleFeatureTypes = simpleFeatureTypes;
     this.filter = filter;
     this.keysInt = keysInt;
     this.includeTags = includeTags;
     this.includeOSMMetadata = includeOSMMetadata;
     this.includeContributionTypes = includeContributionTypes;
-    this.elementsGeometry = elementsGeometry;
+    this.outputGeometry = outputGeometry;
     this.endTimestamp = endTimestamp;
     this.isContainingSimpleFeatureTypes = isContainingSimpleFeatureTypes;
   }
@@ -156,7 +153,7 @@ public class DataExtractionTransformer implements Serializable {
             }
             output.add(exeUtils.createOSMFeature(currentEntity, currentGeom, properties, keysInt,
                 includeTags, includeOSMMetadata, includeContributionTypes, isContributionsEndpoint,
-                elementsGeometry, contribution.getContributionTypes()));
+                outputGeometry, contribution.getContributionTypes()));
           }
         }
         skipNext = false;
@@ -191,7 +188,7 @@ public class DataExtractionTransformer implements Serializable {
         if (addToOutput) {
           output.add(exeUtils.createOSMFeature(currentEntity, currentGeom, properties, keysInt,
               includeTags, includeOSMMetadata, includeContributionTypes, isContributionsEndpoint,
-              elementsGeometry, lastContribution.getContributionTypes()));
+              outputGeometry, lastContribution.getContributionTypes()));
         }
       }
     } else if (isContributionsEndpoint) {
@@ -201,7 +198,7 @@ public class DataExtractionTransformer implements Serializable {
       properties.put("@timestamp",
           TimestampFormatter.getInstance().isoDateTime(lastContribution.getTimestamp()));
       output.add(exeUtils.createOSMFeature(currentEntity, currentGeom, properties, keysInt, false,
-          includeOSMMetadata, includeContributionTypes, isContributionsEndpoint, elementsGeometry,
+          includeOSMMetadata, includeContributionTypes, isContributionsEndpoint, outputGeometry,
           lastContribution.getContributionTypes()));
     }
     return output;
@@ -240,7 +237,7 @@ public class DataExtractionTransformer implements Serializable {
     if (addToOutput) {
       return Collections.singletonList(exeUtils.createOSMFeature(entity, geom, properties,
           keysInt, includeTags, includeOSMMetadata, includeContributionTypes,
-          isContributionsEndpoint, elementsGeometry, EnumSet.noneOf(ContributionType.class)));
+          isContributionsEndpoint, outputGeometry, EnumSet.noneOf(ContributionType.class)));
     } else {
       return Collections.emptyList();
     }
@@ -249,7 +246,7 @@ public class DataExtractionTransformer implements Serializable {
   /** Checks whether the given entity should be added to the output (true) or not (false). */
   private boolean addEntityToOutput(OSMEntity currentEntity, Geometry currentGeom) {
     if (isContainingSimpleFeatureTypes) {
-      return utils.checkGeometryOnSimpleFeatures(currentGeom, simpleFeatureTypes);
+      return inputUtils.checkGeometryOnSimpleFeatures(currentGeom, simpleFeatureTypes);
     } else {
       return filter == null || filter.applyOSMGeometry(currentEntity, currentGeom);
     }
