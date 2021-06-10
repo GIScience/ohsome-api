687030dd9e6f66a55ef1984078316747b020baac
Rosario Trischitta <rosario.trischitta@heigit.org>
2021-06-10 11:48:02 +0200

    chore: add comment to setFullHistory()

--- src/main/lombok/org/heigit/ohsome/ohsomeapi/executor/ContributionsExecutor.java
+++ src/main/lombok/org/heigit/ohsome/ohsomeapi/executor/ContributionsExecutor.java
@@ -68,6 +68,8 @@ public class ContributionsExecutor extends RequestExecutor {
     MapReducer<OSMContribution> mapRed;
     final SortedMap<OSHDBTimestamp, ? extends Number> result;
     if (isContributionsLatestCount) {
+      // otherwise the MapReducer will be filtered in the Inputprocessor
+      // preventing the call of groupByEntity() in contributionsCount()
       inputProcessor.getProcessingData().setFullHistory(true);
     }
     if (DbConnData.db instanceof OSHDBIgnite) {
