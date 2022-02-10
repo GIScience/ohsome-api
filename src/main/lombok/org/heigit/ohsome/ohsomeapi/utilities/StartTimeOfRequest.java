e6c00b0eedc0c6a454949d418b14f272ab8544db
Rosario Trischitta <rosario.trischitta@heigit.org>
2022-02-10 14:41:35 +0100

    add bean for start time of the request

---
+++ src/main/lombok/org/heigit/ohsome/ohsomeapi/utilities/StartTimeOfRequest.java
@@ -0,0 +1,14 @@
+package org.heigit.ohsome.ohsomeapi.utilities;
+
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+@Component
+@RequestScope
+@Getter
+public class StartTimeOfRequest {
+
+  private final long START_TIME = System.currentTimeMillis();
+
+}
