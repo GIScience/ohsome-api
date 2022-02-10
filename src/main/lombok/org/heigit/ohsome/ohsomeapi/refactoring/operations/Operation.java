48d40914ebaf2235361c3a97b6797e950f126241
Rosario Trischitta <rosario.trischitta@heigit.org>
2022-02-10 15:16:21 +0100

    add method declaration

--- src/main/lombok/org/heigit/ohsome/ohsomeapi/refactoring/operations/Operation.java
+++ src/main/lombok/org/heigit/ohsome/ohsomeapi/refactoring/operations/Operation.java
@@ -1,6 +1,7 @@
 package org.heigit.ohsome.ohsomeapi.refactoring.operations;
 
 import java.util.List;
+import org.heigit.ohsome.ohsomeapi.inputprocessing.InputProcessor;
 import org.heigit.ohsome.ohsomeapi.output.Response;
 import org.heigit.ohsome.ohsomeapi.output.Result;
 import org.springframework.stereotype.Component;
@@ -8,11 +9,15 @@ import org.springframework.stereotype.Component;
 @Component
 public interface Operation <T> {
 
-  T compute() throws Exception;
+  Object compute() throws Exception;
 
   Response getResponse(List<? extends Result> resultSet);
 
   String getDescription();
 
   String getUnit();
+
+  String getMetadataDescription();
+
+  InputProcessor getInputProcessor();
 }
