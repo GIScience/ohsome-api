cef7d1b7081fae4498852dea483b185bb9cc7f4b
Rosario Trischitta <rosario.trischitta@heigit.org>
2022-02-16 10:25:56 +0100

    delete commented code

--- src/main/lombok/org/heigit/ohsome/ohsomeapi/refactoring/operations/aggregation/GroupByBoundary.java
+++ src/main/lombok/org/heigit/ohsome/ohsomeapi/refactoring/operations/aggregation/GroupByBoundary.java
@@ -9,7 +9,6 @@ import java.util.SortedMap;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import lombok.Getter;
-import org.heigit.ohsome.ohsomeapi.exception.BadRequestException;
 import org.heigit.ohsome.ohsomeapi.geometrybuilders.GeometryFrom;
 import org.heigit.ohsome.ohsomeapi.inputprocessing.InputProcessor;
 import org.heigit.ohsome.ohsomeapi.output.Description;
@@ -26,7 +25,6 @@ import org.heigit.ohsome.oshdb.api.generic.OSHDBCombinedIndex;
 import org.heigit.ohsome.oshdb.api.mapreducer.MapAggregator;
 import org.heigit.ohsome.oshdb.api.mapreducer.MapReducer;
 import org.heigit.ohsome.oshdb.filter.FilterExpression;
-import org.heigit.ohsome.oshdb.util.function.SerializableFunction;
 import org.heigit.ohsome.oshdb.util.mappable.OSMEntitySnapshot;
 import org.locationtech.jts.geom.Geometry;
 import org.locationtech.jts.geom.Polygonal;
@@ -98,50 +96,6 @@ public class GroupByBoundary implements Operation {
     }
     return mapAgg;
   }
-
-  /**
-   * Computes the result for the /count|length|perimeter|area/groupBy/boundary resources.
-   *
-   * @throws BadRequestException if a boundary parameter is not defined.
-   * @throws Exception thrown by
-   *         {@link org.heigit.ohsome.oshdb.api.mapreducer.MapAggregator#count() count}, or
-   *         {@link org.heigit.ohsome.oshdb.api.mapreducer.MapAggregator#sum(SerializableFunction)
-   *         sum}
-   */
-//  public <P extends Geometry & Polygonal> SortedMap<OSHDBCombinedIndex<OSHDBTimestamp, Integer>,
-//      ? extends Number> computeCountLengthPerimeterAreaGbB(Operation operation, MapReducer<OSMEntitySnapshot> mapRed) throws Exception {
-//    List<Geometry> boundaries = new ArrayList<>(geometryFrom.getGeometryList());
-//    @SuppressWarnings("unchecked") // intentionally as check for P on Polygonal is already performed
-//    Map<Integer, P> geometries = IntStream.range(0, boundaries.size()).boxed()
-//        .collect(Collectors.toMap(idx -> idx, idx -> (P) boundaries.get(idx)));
-//    MapAggregator<OSHDBCombinedIndex<OSHDBTimestamp, Integer>, OSMEntitySnapshot> mapAgg =
-//        mapRed.aggregateByTimestamp().aggregateByGeometry(geometries);
-//    if (inputProcessor.getProcessingData().isContainingSimpleFeatureTypes()) {
-//      mapAgg = inputProcessor.filterOnSimpleFeatures(mapAgg);
-//    }
-//    Optional<FilterExpression> filter = inputProcessor.getProcessingData().getFilterExpression();
-//    if (filter.isPresent()) {
-//      mapAgg = mapAgg.filter(filter.get());
-//    }
-//    var mapAggGeom = mapAgg.map(OSMEntitySnapshot::getGeometry);
-//    if (operation instanceof Count) {
-//      return mapAgg.count();
-//    } else if (operation instanceof Perimeter) {
-//      return mapAggGeom.sum(geom -> {
-//        if (!(geom instanceof Polygonal)) {
-//          return 0.0;
-//        }
-//        return executionUtils.cacheInUserData(geom, () -> Geo.lengthOf(geom.getBoundary()));
-//      });
-//    } else if (operation instanceof Length) {
-//      return mapAggGeom
-//          .sum(geom -> executionUtils.cacheInUserData(geom, () -> Geo.lengthOf(geom)));
-//    } else if (operation instanceof Area) {
-//      return mapAggGeom.sum(geom -> executionUtils.cacheInUserData(geom, () -> Geo.areaOf(geom)));
-//    } else {
-//      return null;
-//    }
-//  }
 
   @Override
   public Response getResponse(List resultSet) {
