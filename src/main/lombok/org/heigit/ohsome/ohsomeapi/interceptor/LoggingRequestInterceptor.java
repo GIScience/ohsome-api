7f618d8621e71f1527635631097acb0a222643fb
Martin Raifer <martin.raifer@heigit.org>
2023-07-05 15:14:03 +0200

    log query boundary bbox

--- src/main/lombok/org/heigit/ohsome/ohsomeapi/interceptor/LoggingRequestInterceptor.java
+++ src/main/lombok/org/heigit/ohsome/ohsomeapi/interceptor/LoggingRequestInterceptor.java
@@ -3,6 +3,8 @@ package org.heigit.ohsome.ohsomeapi.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.heigit.ohsome.ohsomeapi.Application;
+import org.heigit.ohsome.ohsomeapi.inputprocessing.InputProcessor;
+import org.heigit.ohsome.oshdb.util.geometry.OSHDBGeometryBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -32,6 +34,16 @@ public class LoggingRequestInterceptor extends HandlerInterceptorAdapter {
     }
     logger.info("accessed URI: " + requestUri);
     logger.info("query filter: " + request.getParameterMap().getOrDefault("filter", new String[] { "<none>" })[0]);
+    try {
+      var inputProcessor = new InputProcessor(request, false, false);
+      inputProcessor.processParameters();
+      var boundary = inputProcessor.getGeometry();
+      var envelope = boundary.getEnvelopeInternal();
+      var bbox = OSHDBGeometryBuilder.boundingBoxOf(envelope);
+      logger.info("query boundary bbox: " + bbox.toString());
+    } catch (Exception e) {
+      logger.info("query boundary bbox: <error while processing request params>");
+    }
     logger.info("processing time: " + (System.currentTimeMillis() - startTime));
     logger.info("response code: " + response.getStatus());
   }
