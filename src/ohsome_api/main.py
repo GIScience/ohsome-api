456d971ff2d084e8181609ae641f120920d41d88
Martin Raifer <martin.raifer@heigit.org>
2026-03-12 12:14:56 +0100

    document root endpoint (in swagger)
    Co-authored-by: Rafael Troilo <rafael.troilo@heigit.org>
    Co-authored-by: Matthias Schaub <matthias.schaub@heigit.org>
    Co-authored-by: Matthias Merdes <matthias.merdes@heigit.org>

--- src/ohsome_api/main.py
+++ src/ohsome_api/main.py
@@ -6,7 +6,8 @@ app = FastAPI()
 logger = logging.getLogger(__name__)
 
 
-@app.get("/")
+@app.get("/", name="Hello World endpoint")
 def read_root() -> dict:
+    """Returns Hello World as a key/value pair"""
     logger.info("hello world request")
     return {"Hello": "World"}
