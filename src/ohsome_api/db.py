c4f5dd05a6ca1a88c3a0675833342c7a25c9a563
Matthias Schaub <matthias.schaub@heigit.org>
2026-04-01 17:04:54 +0200

    feat: make db connection configurable via env
    Co-authored-by: Rafael Troilo <rafael.troilo@heigit.org>
    Co-authored-by: Martin Raifer <martin.raifer@heigit.org>
    Co-authored-by: Matthias Merdes <matthias.merdes@heigit.org>
    Co-authored-by: Benjamin Herfort <benjamin.herfort@heigit.org>

--- src/ohsome_api/db.py
+++ src/ohsome_api/db.py
@@ -2,8 +2,38 @@ from datetime import datetime
 
 import psycopg
 from psycopg.sql import SQL
+from pydantic import computed_field
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
-CONNECTION_STRING: str = ""
+
+class Config(BaseSettings):
+    user: str = "ohsomedb"
+    password: str = "ohsomedb"  # noqa: S105
+    host: str = "localhost"
+    port: int = 5432
+    dbname: str = "ohsomedb"
+    schemaname: str = "current"
+    application_name: str = "ohsome-api"
+
+    model_config = SettingsConfigDict(
+        env_prefix="OHSOME_API_OHSOMEDB_",
+        env_nested_delimiter="_",
+    )
+
+    @computed_field
+    @property
+    def connection_string(self) -> str:
+        return "postgresql://{user}:{password}@{host}:{port}/{dbname}?application_name={application_name}".format(
+            user=self.user,
+            password=self.password,
+            host=self.host,
+            port=self.port,
+            dbname=self.dbname,
+            application_name=self.application_name,
+        )
+
+
+CONNECTION_STRING: str = Config().connection_string
 
 
 def fetch_one(sql: SQL) -> tuple:
