d5ab3c8ea06928e94a18225745ae4bffbe8df44d
Martin Raifer <martin.raifer@heigit.org>
2026-04-01 13:36:55 +0200

    tests: use metadata request in logger test
    Co-authored-by: Matthias Schaub <matthias.schaub@heigit.org>
    Co-authored-by: Matthias Merdes <matthias.merdes@heigit.org>
    Co-authored-by: Rafael Troilo <rafael.troilo@heigit.org>

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -9,5 +9,5 @@ from ohsome_api import main
 @pytest.mark.usefixtures("ohsomedb_testcontainer")
 async def test_logging(caplog: pytest.LogCaptureFixture):
     caplog.set_level(logging.INFO)
-    await main.read_root()
-    assert "hello world request" in caplog.text
+    await main.get_metadata()
+    assert "Get metadata from ohsomedb." in caplog.text
