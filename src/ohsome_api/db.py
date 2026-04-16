4189d3ccefd809c18e0549c0ce4a4e2154b3f64e
Matthias Merdes <matthias.merdes@heigit.org>
2026-04-16 15:19:29 +0200

    refactor: cleanup sql construction and fix type hints
    Co-authored-by: Martin Raifer <martin.raifer@heigit.org>
    Co-authored-by: Matthias Schaub <matthias.schaub@heigit.org>
    Co-authored-by: Rafael Troilo <rafael.troilo@heigit.org>

--- src/ohsome_api/db.py
+++ src/ohsome_api/db.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Any
 
 import asyncpg
 from asyncpg import Connection, Record
@@ -37,7 +38,7 @@ CONFIG = Config()
 CONNECTION_STRING: str = CONFIG.connection_string
 
 
-async def fetch_row(sql: str, *args: str | datetime) -> Record:
+async def fetch_row(sql: str, *args: Any) -> Record:  # noqa: ANN401
     # TODO: implement a resource manager for db connection
     connection: Connection = await asyncpg.connect(CONNECTION_STRING)
     record: Record = await connection.fetchrow(sql, *args)
@@ -57,16 +58,17 @@ async def get_latest_timestamp() -> datetime:
 
 
 async def get_contributions_count(
-    query_where_clause: str,
-    query_args: tuple,
+    filter_where_clause: str,
+    filter_args: tuple,
     start: datetime,
     end: datetime,
 ) -> int:
+    filter_args_count = len(filter_args)
     sql = f"""
         SELECT COUNT(*) as count
         FROM {CONFIG.schemaname}.contributions
-        WHERE {query_where_clause}
-        AND valid_from BETWEEN ${len(query_args) + 1} AND ${len(query_args) + 2}
+        WHERE {filter_where_clause}
+        AND valid_from BETWEEN ${filter_args_count + 1} AND ${filter_args_count + 2}
     """  # noqa: S608
-    record = await fetch_row(sql, *query_args, start, end)
+    record = await fetch_row(sql, *filter_args, start, end)  # order matters!
     return record["count"]
