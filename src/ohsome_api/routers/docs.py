74259e07054824b14380d9f2f61cf956cdf683e0
Matthias Schaub <matthias.schaub@heigit.org>
2026-06-09 13:46:25 +0200

    revert changes related to url path prefix configuration
    in favor of `fastapi --root-path` option

--- src/ohsome_api/routers/docs.py
+++ src/ohsome_api/routers/docs.py
@@ -4,17 +4,15 @@ from fastapi.openapi.docs import (
 )
 from fastapi.responses import HTMLResponse
 
-from ohsome_api.config import CONFIG
-
 router = APIRouter()
 
 
 @router.get("/docs", include_in_schema=False)
 async def custom_swagger_ui_html() -> HTMLResponse:
     return get_swagger_ui_html(
-        openapi_url=f"{CONFIG.url_path_prefix}/openapi.json",
+        openapi_url="/openapi.json",
         title="ohsome-api - Swagger UI",
         oauth2_redirect_url=None,
-        swagger_js_url=f"{CONFIG.url_path_prefix}/static/swagger-ui-bundle.js",
-        swagger_css_url=f"{CONFIG.url_path_prefix}/static/swagger-ui.css",
+        swagger_js_url="/static/swagger-ui-bundle.js",
+        swagger_css_url="/static/swagger-ui.css",
     )
