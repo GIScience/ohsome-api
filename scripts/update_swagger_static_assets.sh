9ff66a7c6b183836cdb96eb58137b894465f6933
Matthias Schaub <matthias.schaub@heigit.org>
2026-06-09 07:47:45 +0200

    remove redoc static assets

--- scripts/update_swagger_static_assets.sh
+++ scripts/update_swagger_static_assets.sh
@@ -3,5 +3,4 @@
 
 cd "$(git rev-parse --show-toplevel)/static" && \
   wget https://cdn.jsdelivr.net/npm/swagger-ui-dist@5/swagger-ui-bundle.js -O swagger-ui-bundle.js && \
-  wget https://cdn.jsdelivr.net/npm/swagger-ui-dist@5/swagger-ui.css -O swagger-ui.css && \
-  wget https://cdn.jsdelivr.net/npm/redoc@2/bundles/redoc.standalone.js -O redoc.standalone.js
+  wget https://cdn.jsdelivr.net/npm/swagger-ui-dist@5/swagger-ui.css -O swagger-ui.css
