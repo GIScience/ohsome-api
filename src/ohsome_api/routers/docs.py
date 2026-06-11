c565e45d681f77b85faf8ac4d743ab0270889619
Matthias Schaub <matthias.schaub@heigit.org>
2026-06-11 15:51:33 +0200

    api: prefix url for swagger assets with root_path

--- src/ohsome_api/routers/docs.py
+++ src/ohsome_api/routers/docs.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from fastapi.openapi.docs import (
     get_swagger_ui_html,
 )
@@ -8,11 +8,12 @@ router = APIRouter()
 
 
 @router.get("/docs", include_in_schema=False)
-async def custom_swagger_ui_html() -> HTMLResponse:
+async def custom_swagger_ui_html(request: Request) -> HTMLResponse:
+    root_path = request.scope.get("root_path")
     return get_swagger_ui_html(
-        openapi_url="/openapi.json",
+        openapi_url=f"{root_path}/openapi.json",
         title="ohsome-api - Swagger UI",
         oauth2_redirect_url=None,
-        swagger_js_url="/static/swagger-ui-bundle.js",
-        swagger_css_url="/static/swagger-ui.css",
+        swagger_js_url=f"{root_path}/static/swagger-ui-bundle.js",
+        swagger_css_url=f"{root_path}/static/swagger-ui.css",
     )
