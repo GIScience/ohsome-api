f64ef664e788d465378d55a5ba039a47c8a1cddb
Matthias Schaub <matthias.schaub@heigit.org>
2026-06-17 15:47:14 +0200

    extraction: add extraction row model

--- src/ohsome_api/models.py
+++ src/ohsome_api/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Protocol, TypedDict
 
 from pydantic import BaseModel
 
@@ -13,3 +14,26 @@ class TimeBinsRowModel(BaseModel):
 class FeaturesRowModel(BaseModel):
     value: int
     timestamp: datetime
+
+
+# TODO: Use Pydantic Model to make sure openapi schema is generated?
+class ExtractionRow(TypedDict):
+    osm_type: str
+    osm_id: int
+    valid_from: datetime
+    osm_version: int
+    osm_edits: int
+    user_id: int
+    user_name: str
+    changeset_id: int
+    tags: dict[str, str]
+    xmin: float
+    xmax: float
+    ymin: float
+    ymax: float
+    geom: bytes
+
+
+class ExtractionWriter(Protocol):
+    def write_batch(self, batch: list[ExtractionRow]) -> None: ...
+    def close(self) -> None: ...
