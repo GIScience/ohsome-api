b9e8c35c6384ad748ebe123d951f23c633a721b9
Johannes Visintini <johannes.visintini@heigit.org>
2026-06-23 09:52:37 +0200

    api: use externally served static files for swagger UI
    Co-authored-by: Matthias Schaub <matthias.schaub@heigit.org>

--- src/ohsome_api/routers/docs.py
+++ src/ohsome_api/routers/docs.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter
 from fastapi.openapi.docs import (
     get_swagger_ui_html,
 )
@@ -8,13 +8,11 @@ router = APIRouter()
 
 
 @router.get("/docs", include_in_schema=False)
-async def custom_swagger_ui_html(request: Request) -> HTMLResponse:
-    # root_path = request.scope.get("root_path")
-    root_path = ""
+async def custom_swagger_ui_html() -> HTMLResponse:
     return get_swagger_ui_html(
-        openapi_url=f"{root_path}/openapi.json",
+        openapi_url="openapi.json",
         title="ohsome-api - Swagger UI",
         oauth2_redirect_url=None,
-        swagger_js_url=f"{root_path}/static/swagger-ui-bundle.js",
-        swagger_css_url=f"{root_path}/static/swagger-ui.css",
+        swagger_js_url="https://ohsome.org/static/swagger-ui-bundle.js",
+        swagger_css_url="https://ohsome.org/static/swagger-ui.css",
     )
