0fcb7b6312bc88a432c2e0b60c1980253c59c572
Matthias Schaub <matthias.schaub@heigit.org>
2026-06-25 13:56:58 +0200

    docs/swagger: link to account.heigit.org for retrieving an api key

--- src/ohsome_api/dependencies.py
+++ src/ohsome_api/dependencies.py
@@ -1,3 +1,11 @@
 from fastapi.security import APIKeyHeader
 
-api_key_header_scheme = APIKeyHeader(name="authorization", scheme_name="API Key")
+api_key_header_scheme = APIKeyHeader(
+    name="authorization",
+    scheme_name="API Key",
+    description=(
+        "Please enter your "
+        '<a href="https://account.heigit.org/" target="_blank">API key</a>.'
+        "<br><br>"
+    ),
+)
