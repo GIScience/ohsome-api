e388445b6492ce3feb8dc0f79c5826f0cdbbb461
Martin Raifer <martin.raifer@heigit.org>
2026-06-25 14:14:44 +0200

    activity: implement csv endpoint for activity/users
    Co-authored-by: Matthias Schaub <matthias.schaub@heigit.org>

--- src/ohsome_api/routers/activity.py
+++ src/ohsome_api/routers/activity.py
@@ -1,3 +1,5 @@
+from importlib.metadata import version
+
 from fastapi import APIRouter, Depends
 from fastapi.responses import JSONResponse
 
@@ -5,7 +7,9 @@ from ohsome_api import service
 from ohsome_api.dependencies import api_key_header_scheme
 from ohsome_api.request_models import TimeBinsParameters
 from ohsome_api.response_models import CountResponseModel
+from ohsome_api.response_renderers import CSVCountResponse
 
+VERSION = version("ohsome-api")
 router = APIRouter(
     dependencies=[Depends(api_key_header_scheme)],
 )
@@ -20,6 +24,40 @@ router = APIRouter(
 async def post_users_activity_as_json(
     parameters: TimeBinsParameters,
 ) -> CountResponseModel:
+    result = await service.get_users_activity(
+        ohsome_filter=parameters.ohsome_filter,
+        start=parameters.time_bins.start,
+        end=parameters.time_bins.end,
+        bin_size=parameters.time_bins.bin_size,
+        aoi_wkt=parameters.aoi.features[0].geometry.wkt,
+    )
+
+    return CountResponseModel(result=result)
+
+
+@router.post(
+    "/activity/users.csv",
+    response_class=CSVCountResponse,
+    responses={
+        200: {
+            "content": {
+                "text/csv": {
+                    "schema": {"type": "string"},
+                    "example": f"""# apiVersion: {VERSION}
+# attribution.url: https://ohsome.org/copyrights
+# attribution.text: © OpenStreetMap contributors
+start;end;value
+1970-01-01T00:00:00Z;1970-02-01T00:00:00Z;0
+""",
+                },
+            },
+        },
+    },
+)
+async def post_users_activity_as_csv(
+    parameters: TimeBinsParameters,
+) -> CountResponseModel:
+    """Active users per time bin."""
     result = await service.get_users_activity(
         ohsome_filter=parameters.ohsome_filter,
         start=parameters.time_bins.start,
--- tests/api/test_activity.py
+++ tests/api/test_activity.py
@@ -1,3 +1,5 @@
+from importlib.metadata import version
+
 from fastapi.testclient import TestClient
 from starlette.status import HTTP_200_OK
 
@@ -15,3 +17,23 @@ def test_activity_users_as_json(client: TestClient, aoi_geojson_heigit: dict):
     assert response.headers["content-type"] == "application/json"
     assert len(response.json()["result"]) == 1
     assert response.json()["result"][0]["value"] == 6
+
+
+def test_activity_users_as_csv(client: TestClient, aoi_geojson_heigit: dict):
+    response = client.post(
+        "/activity/users.csv",
+        json={
+            "filter": "building=* and building!=no and type:way",
+            "timeBins": {"start": "2024-01-01", "end": "2025-12-31"},
+            "aoi": aoi_geojson_heigit,
+        },
+    )
+    assert response.status_code == HTTP_200_OK
+    assert response.headers["content-type"] == "text/csv; charset=utf-8"
+    expected_result = f"""# apiVersion: {version("ohsome-api")}
+# attribution.url: https://ohsome.org/copyrights
+# attribution.text: © OpenStreetMap contributors
+start;end;value
+2024-01-01T00:00:00Z;2025-12-31T00:00:00Z;6
+"""
+    assert response.text == expected_result
