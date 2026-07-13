475263c55fa857dc4186cc43e9731ef144c1428e
Matthias Schaub <matthias.schaub@heigit.org>
2026-07-13 16:05:21 +0200

    add release script

---
+++ scripts/release.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -e
+
+if [ $# -eq 0 ]
+  then
+    echo "Please supply one of the following values as argument:"
+	echo "major, minor, patch, stable, alpha, beta, rc, post, dev"
+	exit
+fi
+
+OLD_VERSION=$(uv version --short)
+
+uv version --bump "$1"
+
+NEW_VERSION=$(uv version --short)
+
+sed --in-place "s/$OLD_VERSION/$NEW_VERSION/g" tests/conftest.py
+
+git add -p pyproject.toml tests/conftest.py
+git add uv.lock
+
+git commit -m "$NEW_VERSION"
+git tag "$NEW_VERSION" -m "$NEW_VERSION"
+git push origin "$NEW_VERSION"
+git push
