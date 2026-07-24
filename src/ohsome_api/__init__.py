0d7471b7bfcd4d6bdb0a62bfb82cf863bdf70c50
Matthias Schaub <matthias.schaub@heigit.org>
2026-07-24 14:19:07 +0200

    logs: drop uvicorn access logs for health endpoint

--- src/ohsome_api/__init__.py
+++ src/ohsome_api/__init__.py
@@ -6,13 +6,26 @@ import yaml
 
 from ohsome_api.config import CONFIG
 
+
+class HealthEndpointFilter(logging.Filter):
+    def filter(self, record: logging.LogRecord) -> bool:
+        """Filter out log entries for health requests."""
+        if record.args and record.args[2] == "/health":  # noqa: SIM103 # type: ignore
+            return False
+        return True
+
+
 with open(CONFIG.log_config) as f:
     config = yaml.safe_load(f)
 
 logging.config.dictConfig(config)
+
 log_level = os.getenv("OHSOME_API_LOG_LEVEL", None)
 if log_level is not None:
     log_level = log_level.upper()
     logging.getLogger().setLevel(log_level)
     logging.getLogger("ohsome-api").setLevel(log_level)
     logging.getLogger("uvicorn").setLevel(log_level)
+    logging.getLogger("uvicorn.access").setLevel(log_level)
+
+logging.getLogger("uvicorn.access").addFilter(HealthEndpointFilter())
