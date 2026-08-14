9eb6db5786921c08ce1ba0ca329dd4dfcd154962
Matthias Schaub <matthias.schaub@heigit.org>
2026-08-14 16:56:41 +0200

    docs(sphinx): add copy button to code blocks

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,7 +40,7 @@ copyright = "{}, {}".format(datetime.today().year, author)  # noqa: A001
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx_tabs.tabs"]
+extensions = ["sphinx_tabs.tabs", "sphinx_copybutton"]
 templates_path = ["_templates"]
 exclude_patterns = []
 
