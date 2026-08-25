9e110dcafa6de6ea3379f230057d6a57c1a86fa7
Rafael Troilo <rafael.troilo@heigit.org>
2026-08-25 17:09:05 +0200

    database: use separate timeout (BatchQueryTimeout) for batch queries to end stream abruptly

--- src/ohsome_api/database.py
+++ src/ohsome_api/database.py
@@ -30,6 +30,10 @@ class QueryTimeoutError(TimeoutError):
         super().__init__(message)
 
 
+class BatchTimeoutError(RuntimeError):
+    pass
+
+
 async def jsonb_codec(connection: Connection) -> None:
     await connection.set_type_codec(
         "jsonb",
@@ -128,7 +132,7 @@ class Database:
                         yield batch
                         batch = []
             except TimeoutError as error:
-                raise QueryTimeoutError() from error
+                raise BatchTimeoutError() from error
 
             yield batch
 
