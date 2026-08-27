c5363eccf4f41b876a9f1b6149fa80c3ac074300
Johannes Visintini <johannes.visintini@heigit.org>
2026-08-27 14:35:49 +0200

    fix: invalid key for API Key in openapi.json
    Co-authored-by: Matthias Schaub <matthias.schaub@heigit.org>

--- src/ohsome_api/dependencies.py
+++ src/ohsome_api/dependencies.py
@@ -2,7 +2,7 @@ from fastapi.security import APIKeyHeader
 
 api_key_header_scheme = APIKeyHeader(
     name="authorization",
-    scheme_name="API Key",
+    scheme_name="APIKey",
     description=(
         "Please enter your "
         '<a href="https://account.heigit.org/" target="_blank">API key</a>.'
