86108a3538d54e2aa9975c5f9e0ca35ebf81ce94
Matthias Schaub <matthias.schaub@heigit.org>
2026-09-02 11:43:30 +0200

    database: remove functions which were factored out to own modules

--- src/ohsome_api/db.py
+++ src/ohsome_api/db.py
@@ -2,15 +2,12 @@ from datetime import datetime
 from pathlib import Path
 from typing import AsyncIterator, Literal, cast
 
-from asyncpg import Record
 from ohsome_filter_to_sql import OhsomeFilter, ohsome_filter_to_sql
 
 from ohsome_api.config import CONFIG
 from ohsome_api.database import db
 from ohsome_api.models import (
     ExtractionRow,
-    MeasureEnum,
-    TimeBinColumns,
 )
 
 QUERIES_DIR = Path(__file__).parent / "queries"
@@ -69,84 +66,6 @@ async def get_latest_timestamp() -> datetime:
 async def get_metadata() -> dict[str, datetime]:
     sql = Path(QUERIES_DIR / "get_metadata.sql").read_text()
     return dict(await db.fetch_row(sql))
-
-
-def zerofill_records_to_time_bin_columns(
-    records: list[Record], series: list[datetime]
-) -> TimeBinColumns:
-    zerofilled_series = {i: 0 for i in range(len(series) - 1)}
-
-    for record in records:
-        zerofilled_series[record["time_bin"] - 1] = record["value"]
-
-    start_timestamps: list[datetime] = [
-        series[time_bin] for time_bin in zerofilled_series
-    ]
-
-    end_timestamps: list[datetime] = [
-        series[time_bin + 1] for time_bin in zerofilled_series
-    ]
-
-    values: list[int] = list(zerofilled_series.values())
-
-    return TimeBinColumns(start=start_timestamps, end=end_timestamps, value=values)
-
-
-def aggregation_clause(measure: MeasureEnum, clip: bool) -> str:
-    match measure:
-        case MeasureEnum.COUNT:
-            return "COUNT(*) AS value"
-        case MeasureEnum.LENGTH:
-            # [m]
-            if not clip:
-                return """
-                ROUND(SUM(c.length)) AS value
-                """
-
-            return """
-            ROUND(
-                SUM(
-                    CASE
-                        WHEN ST_Covers(
-                            aoi.geom,
-                            c.geom
-                        )
-                        THEN c.length -- Use precomputed length from ohsome-planet
-                        ELSE ST_Length(
-                            ST_Intersection(
-                                c.geom,
-                                aoi.geom
-                            )::geography
-                        )
-                    END
-                )
-            ) AS value
-            """
-        case MeasureEnum.AREA:
-            # [m²]
-            if not clip:
-                return """
-                ROUND(SUM(c.area)) AS value
-                """
-            return """
-            ROUND(
-                SUM(
-                    CASE
-                        WHEN ST_Covers(
-                            aoi.geom,
-                            c.geom
-                        )
-                        THEN c.area -- Use precomputed area from ohsome-planet
-                        ELSE ST_Area(
-                            ST_Intersection(
-                                c.geom,
-                                aoi.geom
-                            )::geography
-                        )
-                    END
-                )
-            ) AS value
-            """
 
 
 async def extract_contributions(
