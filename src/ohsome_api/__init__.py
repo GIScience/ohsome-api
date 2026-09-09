5b32e2cd52d9402917024dbd2435e4f9d7ff1a9d
Matthias Schaub <matthias.schaub@heigit.org>
2026-09-09 11:43:36 +0200

    logging: remove filter class which is not in use

--- src/ohsome_api/__init__.py
+++ src/ohsome_api/__init__.py
@@ -6,15 +6,6 @@ import yaml
 
 from ohsome_api.config import CONFIG
 
-
-class HealthEndpointFilter(logging.Filter):
-    def filter(self, record: logging.LogRecord) -> bool:
-        """Filter out log entries for health requests."""
-        if record.args and record.args[2] == "/health":  # noqa: SIM103 # type: ignore
-            return False
-        return True
-
-
 with open(CONFIG.log_config) as f:
     config = yaml.safe_load(f)
 
